b2a24b2ca132ac646e1829a56aff4eef1cfd1c7d
Alix ANNERAUD <alix@anneraud.fr>
2024-06-02 14:26:55 +0200

    Write integration test for virtual machine file system bindings

---
+++ Modules/Bindings/Tests/WASM_test/src/main.rs
@@ -0,0 +1,37 @@
+#![allow(non_snake_case)]
+
+use Binding_tool::Bind_function_WASM;
+
+#[Bind_function_WASM]
+fn Open_file(Path: &str, Mode: u32, File_identifier: &mut u16) -> u32 {}
+
+#[Bind_function_WASM]
+fn Read_file(File_identifier: u16, Buffer: &mut [u8], Read_size: &mut u32) -> u32 {}
+
+fn main() -> Result<(), ()> {
+    let mut File_identifier = 1;
+
+    let Result = Open_file("test.txt", 1, &mut File_identifier);
+
+    if Result != 0 {
+        return Err(());
+    }
+
+    let mut Buffer = [0; 1024];
+
+    let mut Read_size = 0;
+
+    let Result = Read_file(File_identifier, &mut Buffer, &mut Read_size);
+
+    if Result != 0 {
+        return Err(());
+    }
+
+    let String = String::from_utf8_lossy(&Buffer[..Read_size as usize]);
+
+    if String != "Hello World!" {
+        return Err(());
+    }
+
+    Ok(())
+}
