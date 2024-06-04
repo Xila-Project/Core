534e3dbe383ed48a99e84f9cd9147d131ee7df79
Alix ANNERAUD <alix@anneraud.fr>
2024-06-04 15:22:44 +0200

    Remove screen and graphics from main as module

--- src/main.rs
+++ src/main.rs
@@ -12,8 +12,6 @@ use esp_idf_hal::{
 #[cfg(target_os = "espidf")]
 use esp_idf_sys as _; // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
 
-mod Graphics;
-mod Screen;
 mod Task;
 mod Users;
 use Virtual_machine;
