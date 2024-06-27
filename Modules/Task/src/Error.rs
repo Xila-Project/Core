b479acc8f120abad40367c1eae26ff2be776293b
Alix ANNERAUD <alix@anneraud.fr>
2024-06-26 21:49:02 -0400

    Add error kind to task error_type and add result own type

--- Modules/Task/src/Error.rs
+++ Modules/Task/src/Error.rs
@@ -1,7 +1,20 @@
+use std::sync::PoisonError;
+
+pub type Result<T> = std::result::Result<T, Error_type>;
+
 #[derive(Debug, Clone)]
 pub enum Error_type {
     Invalid_task_identifier,
     Failed_to_create_thread,
     No_thread_for_task,
     Failed_to_spawn_thread,
+    Poisoned_lock,
+    Invalid_environment_variable,
+    Too_many_tasks,
+}
+
+impl<T> From<PoisonError<T>> for Error_type {
+    fn from(_: PoisonError<T>) -> Self {
+        Error_type::Poisoned_lock
+    }
 }
