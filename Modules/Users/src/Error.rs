10272265a125558ef7b7668e7234043fa946c30b
Alix ANNERAUD <alix@anneraud.fr>
2024-06-26 21:20:29 -0400

    Add error kind in users error_type and add result type

--- Modules/Users/src/Error.rs
+++ Modules/Users/src/Error.rs
@@ -1,6 +1,21 @@
+use std::sync::PoisonError;
+
+pub type Result<T> = std::result::Result<T, Error_type>;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(C)]
 pub enum Error_type {
     Duplicate_group_identifier,
     Duplicate_user_identifier,
-    No_group_identifier,
-    No_user_identifier,
+    Invalid_group_identifier,
+    Invalid_user_identifier,
+    Too_many_groups,
+    Too_many_users,
+    Poisoned_lock,
+}
+
+impl<T> From<PoisonError<T>> for Error_type {
+    fn from(_: PoisonError<T>) -> Self {
+        Error_type::Poisoned_lock
+    }
 }
