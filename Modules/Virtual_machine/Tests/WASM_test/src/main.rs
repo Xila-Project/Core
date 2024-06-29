c53eec0005feeab92e00d82147833aa3c48a7a7f
Alix ANNERAUD <alix@anneraud.fr>
2024-06-29 15:23:40 -0400

    Fix warnings in virtual machine integration test

--- Modules/Virtual_machine/Tests/WASM_test/src/main.rs
+++ Modules/Virtual_machine/Tests/WASM_test/src/main.rs
@@ -1,6 +1,8 @@
 #![allow(non_snake_case)]
+#![allow(non_camel_case_types)]
+#![allow(non_upper_case_globals)]
 
-use std::{collections::HashMap, fmt::Pointer, os::raw::c_void};
+use std::collections::HashMap;
 
 #[link(wasm_import_module = "host")]
 extern "C" {
