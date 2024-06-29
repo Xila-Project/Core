aa765402e6398ec15e031f80ae18835742277196
Alix ANNERAUD <alix@anneraud.fr>
2024-06-29 15:08:27 -0400

    Delete discriminant module from shared

--- Modules/Shared/src/lib.rs
+++ Modules/Shared/src/lib.rs
@@ -1,14 +1,12 @@
 #![allow(non_snake_case)]
 #![allow(non_camel_case_types)]
 
-mod Discriminant;
 mod Error;
 mod Mutable_slice;
 mod Mutable_string;
 mod Ring_buffer;
 mod Size;
 
-pub use Discriminant::*;
 pub use Error::*;
 pub use Mutable_slice::*;
 pub use Mutable_string::*;
