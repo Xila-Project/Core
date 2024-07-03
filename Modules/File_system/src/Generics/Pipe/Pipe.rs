759a59d1cd70756cc578020593d33c962f122d18
Alix ANNERAUD <alix@anneraud.fr>
2024-07-02 23:31:39 -0400

    Use VecDeque for Pipe_type instead of Ring_buffer_type for Pipe

--- Modules/File_system/src/Generics/Pipe/Pipe.rs
+++ Modules/File_system/src/Generics/Pipe/Pipe.rs
@@ -1,49 +1,49 @@
-use std::sync::{Arc, RwLock};
-
-use Shared::Ring_buffer_type;
+use std::{
+    collections::VecDeque,
+    sync::{Arc, RwLock},
+};
 
 use crate::Prelude::{Error_type, Result, Size_type};
 
 /// A pipe is a FIFO (ring) buffer that can be used to communicate between tasks.
 #[derive(Clone)]
-pub struct Pipe_type(Arc<RwLock<Ring_buffer_type<u8>>>);
+pub struct Pipe_type(Arc<RwLock<VecDeque<u8>>>);
 
 impl Pipe_type {
     /// Create a new pipe with a buffer of the specified size.
     pub fn New(Buffer_size: usize) -> Self {
-        Self(Arc::new(RwLock::new(Ring_buffer_type::New(Buffer_size))))
+        Self(Arc::new(RwLock::new(VecDeque::with_capacity(Buffer_size))))
     }
 
     pub fn Write(&self, Data: &[u8]) -> Result<()> {
         let mut Inner = self.0.write()?;
 
-        if Data.len() > Inner.Get_free_space() {
+        if Data.len() > Inner.capacity() - Inner.len() {
             return Err(Error_type::File_system_full);
         }
 
         for Byte in Data {
-            if !Inner.Push(*Byte) {
-                return Err(Error_type::File_system_full);
-            }
+            Inner.push_back(*Byte);
         }
+
         Ok(())
     }
 
     pub fn Read(&self, Data: &mut [u8]) -> Result<()> {
         let mut Inner = self.0.write()?;
-        let Length = Data.len();
 
-        if Length > Inner.Get_used_space() {
+        if Data.len() > Inner.len() {
             return Err(Error_type::File_system_full);
         }
 
         for Byte in Data {
-            *Byte = Inner.Pop().unwrap();
+            *Byte = Inner.pop_front().unwrap();
         }
+
         Ok(())
     }
 
     pub fn Get_size(&self) -> Result<Size_type> {
-        Ok(self.0.read()?.Get_capacity().into())
+        Ok(self.0.read()?.len().into())
     }
 }
