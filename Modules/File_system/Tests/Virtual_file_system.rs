e858e1f1d56f7ada3667c34ba82d91af9168c2bb
Alix ANNERAUD <alix@anneraud.fr>
2024-07-06 00:40:11 -0400

    Update Virtual file system integration test

--- Modules/File_system/Tests/Virtual_file_system.rs
+++ Modules/File_system/Tests/Virtual_file_system.rs
@@ -1,20 +1,22 @@
 #![allow(non_snake_case)]
+#![allow(non_camel_case_types)]
 
 #[cfg(target_os = "linux")]
 #[test]
-fn Virtual_file_system_test() {
+fn Test_virtual_file_system_file() {
+    use std::sync::RwLock;
+
     use File_system::{
         Drivers::Native::File_system_type,
-        Prelude::{
-            File_type, Mode_type, Path_type, Position_type, Status_type, Virtual_file_system_type,
-        },
+        Prelude::{Device_trait, File_type, Mode_type, Path_type, Position_type, Status_type},
     };
 
     let Task_manager = Task::Manager_type::New();
 
     let Users_manager = Users::Manager_type::New();
 
-    let Virtual_file_system = Virtual_file_system_type::New(Task_manager, Users_manager);
+    let Virtual_file_system = File_system::Initialize(Task_manager, Users_manager)
+        .expect("Failed to initialize file system");
 
     Virtual_file_system
         .Mount(
@@ -39,7 +41,7 @@ fn Virtual_file_system_test() {
         .expect("Failed to create file");
 
     let File = File_type::Open(
-        &Virtual_file_system,
+        Virtual_file_system,
         File_path,
         Mode_type::Read_write().into(),
     )
@@ -49,7 +51,7 @@ fn Virtual_file_system_test() {
 
     File.Write(Data).expect("Failed to write data");
 
-    File.Set_position(&Position_type::Start(0_u64.into()))
+    File.Set_position(&Position_type::Start(0))
         .expect("Failed to set position");
 
     let mut Buffer = [0; 13];
@@ -64,9 +66,12 @@ fn Virtual_file_system_test() {
         .Delete(File_path, false)
         .expect("Failed to delete file");
 
-    let (Pipe_read, Pipe_write) =
-        File_type::Create_unamed_pipe(&Virtual_file_system, 512, Status_type::default())
-            .expect("Failed to create pipe");
+    let (Pipe_read, Pipe_write) = File_type::Create_unnamed_pipe(
+        Virtual_file_system,
+        512_usize.into(),
+        Status_type::default(),
+    )
+    .expect("Failed to create pipe");
 
     Pipe_write.Write(Data).expect("Failed to write data");
 
@@ -87,16 +92,23 @@ fn Virtual_file_system_test() {
             .expect("Failed to delete pipe");
     }
 
-    /*
     Virtual_file_system
-        .Create_named_pipe(&Pipe_path, 512)
+        .Create_named_pipe(&Pipe_path, 512_usize.into())
         .expect("Failed to create pipe");
 
     let Pipe_read = File_type::Open(
-        &Virtual_file_system,
+        Virtual_file_system,
         Pipe_path,
         Mode_type::Read_only().into(),
-    ).expect("Failed to open pipe");
+    )
+    .expect("Failed to open pipe");
+
+    let Pipe_write = File_type::Open(
+        Virtual_file_system,
+        Pipe_path,
+        Mode_type::Write_only().into(),
+    )
+    .expect("Failed to open pipe");
 
     let mut Buffer = [0; 13];
 
@@ -112,5 +124,72 @@ fn Virtual_file_system_test() {
     Virtual_file_system
         .Delete(Pipe_path, false)
         .expect("Failed to delete pipe");
-    */
+
+    struct Dummy_device_type(RwLock<u64>);
+
+    impl Device_trait for Dummy_device_type {
+        fn Read(&self, Buffer: &mut [u8]) -> File_system::Prelude::Result_type<usize> {
+            Buffer.copy_from_slice(&self.0.read()?.to_le_bytes());
+
+            Ok(std::mem::size_of::<u64>())
+        }
+
+        fn Write(&self, Buffer: &[u8]) -> File_system::Prelude::Result_type<usize> {
+            *self.0.write()? = u64::from_le_bytes(Buffer.try_into().unwrap());
+
+            Ok(std::mem::size_of::<u64>())
+        }
+
+        fn Get_size(&self) -> File_system::Prelude::Result_type<usize> {
+            Ok(std::mem::size_of::<u64>())
+        }
+
+        fn Set_position(
+            &self,
+            _: &File_system::Prelude::Position_type,
+        ) -> File_system::Prelude::Result_type<usize> {
+            Ok(0)
+        }
+
+        fn Flush(&self) -> File_system::Prelude::Result_type<()> {
+            Ok(())
+        }
+    }
+
+    let Device = Dummy_device_type(RwLock::new(0));
+
+    let Device_path = Path_type::New("/Device").expect("Failed to create path");
+
+    Virtual_file_system
+        .Add_device(Device_path, Box::new(Device))
+        .expect("Failed to add device");
+
+    let Device_file = File_type::Open(
+        Virtual_file_system,
+        Device_path,
+        Mode_type::Read_write().into(),
+    )
+    .expect("Failed to open device");
+
+    let Data = 0x1234567890ABCDEF_u64;
+
+    Device_file
+        .Write(&Data.to_le_bytes())
+        .expect("Failed to write data");
+
+    Device_file
+        .Set_position(&Position_type::Start(0))
+        .expect("Failed to set position");
+
+    let mut Buffer = [0; 8];
+
+    Device_file.Read(&mut Buffer).expect("Failed to read data");
+
+    assert_eq!(Buffer, Data.to_le_bytes());
+
+    std::mem::drop(Device_file);
+
+    Virtual_file_system
+        .Delete(Device_path, false)
+        .expect("Failed to delete device");
 }
