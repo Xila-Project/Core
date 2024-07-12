16c2aea1dff752f57e8d723a54ff747135a9f8f6
Alix ANNERAUD <alix@anneraud.fr>
2024-07-11 23:55:56 -0400

    Add automatic build for Build_tool when sourcing Export.sh

--- Export.sh
+++ Export.sh
@@ -4,15 +4,22 @@
 Build_tool_directory=$(realpath "Build_tool")
 Build_tool_executable=$Build_tool_directory/target/release/Build_tool
 
+# Check if the 'Build_tool' directory exists
 if [ ! -d "$Build_tool_directory" ]; then
     echo "The 'Build_tool' directory does not exist. Are you in the root of the repository?"
     exit 1
 fi
 
-if [ ! -f "$Build_tool_executable" ]; then
-    echo "The 'Build_tool' executable does not exist. Did you build the project? (Run 'cargo build --release')"
+
+(
+# Build the build tool
+cd $Build_tool_directory && cargo build --release
+
+if [ $? -ne 0 ]; then
+    echo "Failed to build the build tool"
     exit 1
 fi
+)
 
 clear && echo "Export xila build tool"
 
