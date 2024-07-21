122b82abf0c05cf15c71988261b4234f624b0ffe
Alix ANNERAUD <alix@anneraud.fr>
2024-07-21 01:07:48 -0400

    Fix export script

--- Export.sh
+++ Export.sh
@@ -7,7 +7,7 @@ Build_tool_executable=$Build_tool_directory/target/release/Build_tool
 # Check if the 'Build_tool' directory exists
 if [ ! -d "$Build_tool_directory" ]; then
     echo "The 'Build_tool' directory does not exist. Are you in the root of the repository?"
-    exit 1
+    return 1
 fi
 
 
@@ -17,7 +17,7 @@ cd $Build_tool_directory && cargo build --release
 
 if [ $? -ne 0 ]; then
     echo "Failed to build the build tool"
-    exit 1
+    return 1
 fi
 )
 
