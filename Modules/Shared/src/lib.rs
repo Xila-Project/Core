c444552e5759b3d48dd6656ca6fa9a2295f379a1
Alix ANNERAUD <alix@anneraud.fr>
2024-09-23 22:01:12 +0200

    Delete old time module from Shared module

--- Modules/Shared/src/lib.rs
+++ Modules/Shared/src/lib.rs
@@ -6,11 +6,9 @@ mod Mutable_slice;
 mod Mutable_string;
 mod Ring_buffer;
 mod Size;
-mod Time;
 
 pub use Error::*;
 pub use Mutable_slice::*;
 pub use Mutable_string::*;
 pub use Ring_buffer::*;
 pub use Size::*;
-pub use Time::*;
