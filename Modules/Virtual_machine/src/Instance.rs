63d1a9df8c831fb4d2874dc89e7e5a015df14a8b
Alix ANNERAUD <alix@anneraud.fr>
2024-09-25 12:54:50 +0200

    Add allowed directory in wasi arguments

--- Modules/Virtual_machine/src/Instance.rs
+++ Modules/Virtual_machine/src/Instance.rs
@@ -1,7 +1,7 @@
 #![allow(non_snake_case)]
 #![allow(non_camel_case_types)]
 
-use std::ptr::null_mut;
+use std::{ffi::CString, mem::forget, ptr::null_mut};
 
 use wamr_rust_sdk::{
     function::Function, instance::Instance, sys::wasm_runtime_set_wasi_args_ex, value::WasmValue,
@@ -12,7 +12,24 @@ use crate::{
     Data::Data_type, Environment_type, Module::Module_type, Result_type, Runtime::Runtime_type,
 };
 
-pub struct Instance_type(Instance);
+pub struct Instance_type {
+    Instance: Instance,
+    Directory_paths_raw: Vec<*const i8>,
+}
+
+impl Drop for Instance_type {
+    fn drop(&mut self) {
+        unsafe {
+            let _ = self
+                .Directory_paths_raw
+                .iter_mut()
+                .map(|x| CString::from_raw(*x as *mut i8))
+                .collect::<Vec<CString>>();
+
+            // Directory_paths will be dropped here and paths memory will be freed.
+        }
+    }
+}
 
 impl Instance_type {
     pub fn New(
@@ -24,11 +41,18 @@ impl Instance_type {
         Standard_out: Unique_file_identifier_type,
         Standard_error: Unique_file_identifier_type,
     ) -> Result_type<Self> {
+        let mut Directory_paths = [CString::new("/").unwrap()];
+
+        let mut Directory_paths_raw: Vec<*const i8> =
+            Directory_paths.iter().map(|x| x.as_ptr()).collect();
+
+        Directory_paths.iter_mut().for_each(forget);
+
         unsafe {
             wasm_runtime_set_wasi_args_ex(
                 Module.Get_inner_reference().get_inner_module(),
-                null_mut(),
-                0,
+                Directory_paths_raw.as_mut_ptr(),
+                Directory_paths_raw.len() as u32,
                 null_mut(),
                 0,
                 null_mut(),
@@ -41,11 +65,16 @@ impl Instance_type {
             )
         }
 
-        let Instance = Instance_type(Instance::new(
+        let WAMR_instance = Instance::new(
             Runtime.Get_inner_reference(),
             Module.Get_inner_reference(),
             Stack_size as u32,
-        )?);
+        )?;
+
+        let Instance = Instance_type {
+            Instance: WAMR_instance,
+            Directory_paths_raw,
+        };
 
         let mut Execution_environment = Environment_type::From_instance(&Instance)?;
 
@@ -62,12 +91,12 @@ impl Instance_type {
         if Parameters.is_empty() {
             Ok(
                 Function::find_export_func(self.Get_inner_reference(), Name)?
-                    .call(&self.0, &vec![WasmValue::I32(0)])?,
+                    .call(&self.Instance, &vec![WasmValue::I32(0)])?,
             )
         } else {
             Ok(
                 Function::find_export_func(self.Get_inner_reference(), Name)?
-                    .call(&self.0, Parameters)?,
+                    .call(&self.Instance, Parameters)?,
             )
         }
     }
@@ -77,6 +106,6 @@ impl Instance_type {
     }
 
     pub(crate) fn Get_inner_reference(&self) -> &Instance {
-        &self.0
+        &self.Instance
     }
 }
