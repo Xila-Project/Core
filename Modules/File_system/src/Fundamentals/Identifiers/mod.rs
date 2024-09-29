46d738223934eb5848978e3e69ec0acaff703c98
Alix ANNERAUD <alix@anneraud.fr>
2024-09-29 22:44:49 +0200

    Create inode type and module in file system

--- Modules/File_system/src/Fundamentals/Identifiers/mod.rs
+++ Modules/File_system/src/Fundamentals/Identifiers/mod.rs
@@ -3,9 +3,11 @@ mod File;
 mod File_system;
 mod Local_file;
 mod Unique_file;
+mod Inode;
 
 pub use Entry::*;
 pub use File::*;
 pub use File_system::*;
 pub use Local_file::*;
 pub use Unique_file::*;
+pub use Inode::*;
