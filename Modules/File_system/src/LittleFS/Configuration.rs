c60f2f09b0c7634c747ec8e0caa5da52f6bd06b6
Alix ANNERAUD <alix@anneraud.fr>
2024-10-01 10:13:21 +0200

    Refactor little fs configuration

--- Modules/File_system/src/LittleFS/Configuration.rs
+++ Modules/File_system/src/LittleFS/Configuration.rs
@@ -1,4 +1,4 @@
-use std::os::raw::c_void;
+use std::{ffi::c_void, mem::forget};
 
 use crate::Device::Device_type;
 
@@ -6,19 +6,17 @@ use super::{littlefs, Callbacks};
 
 #[derive(Debug, Clone)]
 pub struct Configuration_type {
-    Context: Option<Device_type>,
+    Context: Device_type,
     Read_size: usize,
     Program_size: usize,
     Block_size: usize,
-    Block_count: Option<usize>,
+    Block_count: usize,
     Block_cycles: Option<u16>,
-    Cache_size: Option<usize>,
     Maximum_file_size: Option<usize>,
     Maximum_attributes_size: Option<usize>,
     Maximum_name_size: Option<usize>,
-    Read_buffer: Option<*mut c_void>,
-    Write_buffer: Option<*mut c_void>,
-    Lookahead_buffer: Option<*mut c_void>,
+    Cache_size: usize,
+    Look_ahead_size: usize,
 }
 
 impl Configuration_type {
@@ -43,6 +41,42 @@ impl Configuration_type {
         attr_max: 0,
     };
 
+    pub fn New(
+        Device: Device_type,
+        Block_size: usize,
+        Total_size: usize,
+        Cache_size: usize,
+        Look_ahead_size: usize,
+    ) -> Option<Self> {
+        if (Total_size % Block_size) != 0 {
+            return None;
+        }
+
+        if !(Block_size % Cache_size) == 0 {
+            return None;
+        }
+
+        if (Look_ahead_size % 8) != 0 {
+            return None;
+        }
+
+        let Block_count = Total_size / Block_size;
+
+        Some(Self {
+            Context: Device,
+            Read_size: 16,
+            Program_size: 16,
+            Block_size,
+            Block_count,
+            Block_cycles: None,
+            Maximum_file_size: None,
+            Maximum_attributes_size: None,
+            Maximum_name_size: None,
+            Cache_size,
+            Look_ahead_size,
+        })
+    }
+
     pub const fn Set_read_size(mut self, Read_size: usize) -> Self {
         self.Read_size = Read_size;
         self
@@ -50,16 +84,6 @@ impl Configuration_type {
 
     pub const fn Set_program_size(mut self, Program_size: usize) -> Self {
         self.Program_size = Program_size;
-        self
-    }
-
-    pub const fn Set_block_size(mut self, Block_size: usize) -> Self {
-        self.Block_size = Block_size;
-        self
-    }
-
-    pub const fn Set_block_count(mut self, Block_count: usize) -> Self {
-        self.Block_count = Some(Block_count);
         self
     }
 
@@ -82,54 +106,17 @@ impl Configuration_type {
         self.Maximum_name_size = Some(Maximum_name_size);
         self
     }
-
-    pub(crate) const fn Set_cache_size(mut self, Cache_size: usize) -> Self {
-        self.Cache_size = Some(Cache_size);
-        self
-    }
-
-    pub(crate) fn Set_context(mut self, Device: Device_type) -> Self {
-        self.Context = Some(Device);
-        self
-    }
-    pub(crate) fn Set_buffers(
-        mut self,
-        Read_buffer: *mut u8,
-        Write_buffer: *mut u8,
-        Lookahead_buffer: *mut u8,
-    ) -> Self {
-        self.Read_buffer = Some(Read_buffer as *mut _ as *mut c_void);
-        self.Write_buffer = Some(Write_buffer as *mut _ as *mut c_void);
-        self.Lookahead_buffer = Some(Lookahead_buffer as *mut _ as *mut c_void);
-        self
-    }
-}
-
-impl Default for Configuration_type {
-    fn default() -> Self {
-        Self {
-            Read_size: 16,
-            Program_size: 16,
-            Block_size: 512,
-            Block_count: None,
-            Block_cycles: None,
-            Cache_size: None,
-            Maximum_file_size: None,
-            Maximum_attributes_size: None,
-            Maximum_name_size: None,
-            Context: None,
-            Read_buffer: None,
-            Write_buffer: None,
-            Lookahead_buffer: None,
-        }
-    }
 }
 
 impl TryFrom<Configuration_type> for littlefs::lfs_config {
     type Error = ();
 
     fn try_from(Configuration: Configuration_type) -> Result<Self, Self::Error> {
-        Ok(littlefs::lfs_config {
+        let mut Read_buffer = vec![0_u8; Configuration.Cache_size];
+        let mut Write_buffer = Read_buffer.clone();
+        let mut Look_ahead_buffer = vec![0_u8; Configuration.Look_ahead_size];
+
+        let LFS_Configuration = littlefs::lfs_config {
             context: Box::into_raw(Box::new(Configuration.Context)) as *mut c_void,
             read: Some(Callbacks::Read_callback),
             prog: Some(Callbacks::Programm_callback),
@@ -138,19 +125,25 @@ impl TryFrom<Configuration_type> for littlefs::lfs_config {
             read_size: Configuration.Read_size as u32,
             prog_size: Configuration.Program_size as u32,
             block_size: Configuration.Block_size as u32,
-            block_count: Configuration.Block_count.unwrap_or(0) as u32,
+            block_count: Configuration.Block_count as u32,
             block_cycles: match Configuration.Block_cycles {
                 Some(Block_cycles) => Block_cycles as i32,
                 None => -1,
             },
-            cache_size: Configuration.Cache_size.ok_or(())? as u32,
-            lookahead_size: Configuration.Cache_size.ok_or(())? as u32,
-            read_buffer: Configuration.Read_buffer.ok_or(())?,
-            prog_buffer: Configuration.Write_buffer.ok_or(())?,
-            lookahead_buffer: Configuration.Lookahead_buffer.ok_or(())?,
+            cache_size: Configuration.Cache_size as u32,
+            lookahead_size: Configuration.Look_ahead_size as u32,
+            read_buffer: Read_buffer.as_mut_ptr() as *mut c_void,
+            prog_buffer: Write_buffer.as_mut_ptr() as *mut c_void,
+            lookahead_buffer: Look_ahead_buffer.as_mut_ptr() as *mut c_void,
             name_max: Configuration.Maximum_name_size.unwrap_or(0) as u32, // Default value : 255 (LFS_NAME_MAX)
             file_max: Configuration.Maximum_file_size.unwrap_or(0) as u32, // Default value : 2,147,483,647 (2 GiB) (LFS_FILE_MAX)
             attr_max: Configuration.Maximum_attributes_size.unwrap_or(0) as u32, // Default value : 1022 (LFS_ATTR_MAX)
-        })
+        };
+
+        forget(Read_buffer);
+        forget(Write_buffer);
+        forget(Look_ahead_buffer);
+
+        Ok(LFS_Configuration)
     }
 }
