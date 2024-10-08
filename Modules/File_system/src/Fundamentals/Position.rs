b88a418e990528a840a0c201b00a53d20240e656
Alix ANNERAUD <alix@anneraud.fr>
2024-10-08 12:23:09 +0200

    Add whence type for C representation instead of using position directly

--- Modules/File_system/src/Fundamentals/Position.rs
+++ Modules/File_system/src/Fundamentals/Position.rs
@@ -10,20 +10,26 @@
 /// let Position = Position_type::Start(0);
 /// ```
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(C)]
 pub enum Position_type {
     Start(u64),
     Current(i64),
     End(i64),
 }
 
-#[cfg(feature = "std")]
-impl From<Position_type> for std::io::SeekFrom {
-    fn from(Position: Position_type) -> Self {
-        match Position {
-            Position_type::Start(Item) => std::io::SeekFrom::Start(Item),
-            Position_type::Current(Item) => std::io::SeekFrom::Current(Item),
-            Position_type::End(Item) => std::io::SeekFrom::End(Item),
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(C)]
+pub enum Whence_type {
+    Start,
+    Current,
+    End,
+}
+
+impl Position_type {
+    pub fn From_whence(Whence: Whence_type, Offset: i64) -> Self {
+        match Whence {
+            Whence_type::Start => Position_type::Start(Offset as u64),
+            Whence_type::Current => Position_type::Current(Offset),
+            Whence_type::End => Position_type::End(Offset),
         }
     }
 }
