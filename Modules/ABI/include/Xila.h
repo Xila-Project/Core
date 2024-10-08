bae10899bf26ab67243158b9c71abcbbdd2da5cc
Alix ANNERAUD <alix@anneraud.fr>
2024-10-08 13:05:59 +0200

    Update file system ABI with new file system API

--- Modules/ABI/include/Xila.h
+++ Modules/ABI/include/Xila.h
@@ -14,7 +14,7 @@ int Rust_function Xila_virtual_print_formatted(const char *format, va_list ap);
 // - CPU
 void Rust_function Xila_flush_data_cache();
 void Rust_function Xila_flush_instruction_cache(void *Start, size_t Length);
- 
+
 // - File system
 typedef enum
 {
@@ -60,7 +60,7 @@ enum
     Xila_file_system_type_symbolic_link,
 };
 
-typedef uint8_t File_system_type_type;
+typedef uint8_t Xila_file_system_type_type;
 
 typedef struct
 {
@@ -71,12 +71,21 @@ typedef struct
     uint64_t Last_access;
     uint64_t Last_modification;
     uint64_t Last_status_change;
-    File_system_type_type Type;
+    Xila_file_system_type_type Type;
 } Xila_file_system_statistics_type;
+
+typedef enum
+{
+    Xila_file_system_whence_start,
+    Xila_file_system_whence_current,
+    Xila_file_system_whence_end,
+} Xila_file_system_whence_type;
 
 typedef uint8_t Xila_file_system_mode_type;
 typedef uint8_t Xila_file_system_open_type;
 typedef uint8_t Xila_file_system_status_type;
+typedef uint64_t Xila_file_system_size_type;
+typedef uint64_t Xila_file_system_inode_type;
 
 extern const Xila_file_system_mode_type Xila_file_system_mode_read_mask;
 extern const Xila_file_system_mode_type Xila_file_system_mode_write_mask;
@@ -84,7 +93,6 @@ extern const Xila_file_system_mode_type Xila_file_system_mode_write_mask;
 extern const Xila_file_system_open_type Xila_file_system_open_create_mask;
 extern const Xila_file_system_open_type Xila_file_system_open_create_only_mask;
 extern const Xila_file_system_open_type Xila_file_system_open_truncate_mask;
-extern const Xila_file_system_open_type Xila_file_system_open_directory_mask;
 
 extern const Xila_file_system_status_type Xila_file_system_status_append_mask;
 extern const Xila_file_system_status_type Xila_file_system_status_non_blocking_mask;
@@ -94,16 +102,15 @@ extern const Xila_file_system_status_type Xila_file_system_status_synchronous_da
 typedef uint64_t Xile_file_system_file_identifier_type;
 
 __wasi_errno_t
-Into_Wasi_Error(File_system_result_type Error);
+Into_WASI_Error(File_system_result_type Error);
 void Into_WASI_file_statistics(const Xila_file_system_statistics_type *Statistics, __wasi_filestat_t *WASI_Statistics);
 
-File_system_result_type Rust_function Xila_get_file_statistics(Xile_file_system_file_identifier_type Handle, Xila_file_system_statistics_type *Buffer);
+File_system_result_type Rust_function Xila_file_system_get_statistics(Xile_file_system_file_identifier_type Handle, Xila_file_system_statistics_type *Buffer);
 File_system_result_type Rust_function Xila_get_file_statistics_at(Xile_file_system_file_identifier_type Handle, const char *Path, struct __wasi_filestat_t *Buffer, __wasi_lookupflags_t Flags);
 File_system_result_type Rust_function Xila_get_file_flags(Xile_file_system_file_identifier_type Handle, __wasi_fdflags_t *Flags);
 File_system_result_type Rust_function Xila_set_file_flags(Xile_file_system_file_identifier_type Handle, __wasi_fdflags_t Flags);
-File_system_result_type Rust_function Xila_synchronize_file_data(Xile_file_system_file_identifier_type Handle, bool metadata);
-File_system_result_type Rust_function Xila_file_system_open(const char *Path, Xila_file_system_mode_type Mode, Xila_file_system_open_type Open, Xila_file_system_status_type Status, Xile_file_system_file_identifier_type *Handle);
-File_system_result_type Rust_function Xila_file_system_open_at(Xile_file_system_file_identifier_type Handle, const char *Path, Xila_file_system_mode_type Mode, Xila_file_system_open_type Open, Xila_file_system_status_type Status,  Xile_file_system_file_identifier_type *New_handle);
+File_system_result_type Rust_function Xila_file_system_flush(Xile_file_system_file_identifier_type Handle, bool All);
+File_system_result_type Rust_function Xila_file_system_open(const char *Path, Xila_file_system_mode_type Mode, Xila_file_system_open_type Open, Xila_file_system_status_type Status, Xile_file_system_file_identifier_type *New_handle);
 File_system_result_type Rust_function Xila_file_system_get_access_mode(Xile_file_system_file_identifier_type Handle, uint8_t *Access_mode);
 File_system_result_type Rust_function Xila_file_system_close(Xile_file_system_file_identifier_type Handle);
 File_system_result_type Rust_function Xila_positioned_read_vectored(Xile_file_system_file_identifier_type Handle, const struct __wasi_iovec_t *IOV, int IOV_len, __wasi_filesize_t Offset, size_t *NRead);
@@ -114,21 +121,20 @@ File_system_result_type Rust_function Xila_allocate_file(Xile_file_system_file_i
 File_system_result_type Rust_function Xila_truncate_file(Xile_file_system_file_identifier_type handle, __wasi_filesize_t size);
 File_system_result_type Rust_function Xila_set_file_times(Xile_file_system_file_identifier_type handle, __wasi_timestamp_t ATime, __wasi_timestamp_t MTime, __wasi_fstflags_t Fst_flags);
 File_system_result_type Rust_function Xila_set_file_times_at(Xile_file_system_file_identifier_type handle, const char *Path, size_t Path_length, __wasi_timestamp_t ATime, __wasi_timestamp_t MTime, __wasi_fstflags_t Fst_flags);
-File_system_result_type Rust_function Xila_read_link_at(Xile_file_system_file_identifier_type handle, const char *Path, char *Buffer, size_t Buffer_size, size_t *Buffer_used);
+File_system_result_type Rust_function Xila_file_system_read_link_at(Xile_file_system_file_identifier_type Directory, const char *Path, char *Buffer, size_t Buffer_size, size_t *Buffer_used);
 File_system_result_type Rust_function Xila_create_link_at(Xile_file_system_file_identifier_type Old_handle, const char *Old_path, Xile_file_system_file_identifier_type New_handle, const char *New_path, bool Follow);
 File_system_result_type Rust_function Xila_create_symbolic_link_at(const char *Target_path, Xile_file_system_file_identifier_type New_handle, const char *Link_path);
-File_system_result_type Rust_function Xila_create_directory(Xile_file_system_file_identifier_type Handle, const char *Path);
-File_system_result_type Rust_function Xila_create_directory_at(Xile_file_system_file_identifier_type Handle, const char *Path);
-File_system_result_type Rust_function Xila_rename_at(Xile_file_system_file_identifier_type Old_handle, const char *Old_path, Xile_file_system_file_identifier_type New_handle, const char *New_path);
+File_system_result_type Rust_function Xila_file_system_create_directory(const char *Path);
+File_system_result_type Rust_function Xila_file_system_rename(const char *Old_path, const char *New_path);
 File_system_result_type Rust_function Xila_unlink_at(Xile_file_system_file_identifier_type Handle, const char *Path, bool Is_directory);
-File_system_result_type Rust_function Xila_set_position(Xile_file_system_file_identifier_type Handle, __wasi_filedelta_t Offset, __wasi_whence_t Whence, __wasi_filesize_t *New_offset);
+File_system_result_type Rust_function Xila_file_system_set_position(Xile_file_system_file_identifier_type Handle, int64_t Offset, Xila_file_system_whence_type Whence, Xila_file_system_size_type *New_offset);
 File_system_result_type Rust_function Xila_get_advisory_information(Xile_file_system_file_identifier_type Handle, __wasi_advice_t Advice, __wasi_filesize_t Offset, __wasi_filesize_t Len);
-File_system_result_type Rust_function Xila_file_system_is_terminal(Xile_file_system_file_identifier_type Handle);
-File_system_result_type Rust_function Xila_open_directory(Xile_file_system_file_identifier_type Handle, os_dir_stream *Dir_stream);
-File_system_result_type Rust_function Xila_rewind_directory(os_dir_stream Dir_stream);
-File_system_result_type Rust_function Xila_set_directory_position(os_dir_stream Dir_stream, uint64_t Position);
-File_system_result_type Rust_function Xila_read_directory(os_dir_stream Dir_stream, __wasi_dirent_t *Buffer, const char **Buffer_used);
-File_system_result_type Rust_function Xila_close_directory(os_dir_stream Dir_stream);
+File_system_result_type Rust_function Xila_file_system_is_a_terminal(Xile_file_system_file_identifier_type Handle);
+File_system_result_type Rust_function Xila_file_system_open_directory(const char *Path, Xile_file_system_file_identifier_type *Directory);
+File_system_result_type Rust_function Xila_file_system_rewind_directory(os_dir_stream Dir_stream);
+File_system_result_type Rust_function Xila_file_system_set_directory_position(os_dir_stream Dir_stream, uint64_t Position);
+File_system_result_type Rust_function Xila_file_system_read_directory(Xile_file_system_file_identifier_type Dir_stream, const char **Name, Xila_file_system_type_type *Type, Xila_file_system_size_type *Size, Xila_file_system_inode_type *Inode);
+File_system_result_type Rust_function Xila_file_system_close_directory(os_dir_stream Dir_stream);
 File_system_result_type Xila_file_system_resolve_path(const char *Path, char *Resolved_path, size_t Resolved_path_size);
 bool Rust_function Xila_file_system_is_stdin(Xile_file_system_file_identifier_type File);
 bool Rust_function Xila_file_system_is_stdout(Xile_file_system_file_identifier_type File);
--- Modules/ABI/src/File_system.rs
+++ Modules/ABI/src/File_system.rs
@@ -1,9 +1,15 @@
 /// This module implements the POSIX like file system C ABI.
-use std::{cmp::min, num::NonZeroU32, ptr::copy_nonoverlapping};
+use std::{
+    cmp::min,
+    ffi::{c_char, CStr, CString},
+    num::NonZeroU32,
+    ptr::{copy_nonoverlapping, null_mut},
+};
 
 use File_system::{
     Error_type, File_identifier_type, Flags_type, Get_instance as Get_file_system_instance,
-    Mode_type, Open_type, Result_type, Statistics_type, Status_type, Unique_file_identifier_type,
+    Inode_type, Mode_type, Open_type, Position_type, Result_type, Size_type, Statistics_type,
+    Status_type, Type_type, Unique_file_identifier_type, Whence_type,
 };
 
 use Task::Get_instance as Get_task_manager_instance;
@@ -65,11 +71,15 @@ fn Statistics_from_mutable_pointer(
 ///
 /// This function may return an error if the file system fails to open the file.
 #[no_mangle]
-pub unsafe extern "C" fn Xila_get_file_statistics(
+pub unsafe extern "C" fn Xila_file_system_get_statistics(
     File: Unique_file_identifier_type,
     Statistics: *mut Statistics_type,
 ) -> u32 {
+    println!("Getting file statistics : {:?}", File);
+
     Into_u32(move || {
+        println!("Getting file statistics : {:?}", File);
+
         let Task_identifier = Get_task_manager_instance()
             .Get_current_task_identifier()
             .map_err(|_| Error_type::Failed_to_get_task_informations)?;
@@ -80,6 +90,8 @@ pub unsafe extern "C" fn Xila_get_file_statistics(
         *Statistics = Get_file_system_instance()
             .Get_statistics(File, Task_identifier)
             .expect("Failed to get file statistics.");
+
+        println!("Statistics : {:?}", *Statistics);
 
         Ok(())
     })
@@ -99,6 +111,8 @@ pub unsafe extern "C" fn Xila_file_system_get_access_mode(
     File: Unique_file_identifier_type,
     Mode: *mut Mode_type,
 ) -> u32 {
+    println!("Getting file access mode : {:?}", File);
+
     Into_u32(move || {
         let Task_identifier = Get_task_manager_instance()
             .Get_current_task_identifier()
@@ -237,11 +251,19 @@ pub unsafe extern "C" fn Xila_file_system_is_terminal(
     File: Unique_file_identifier_type,
     Is_a_terminal: *mut bool,
 ) -> u32 {
-    println!("Checking if file is a terminal : {:?}", File);
+    Into_u32(move || {
+        let Task_identifier = Get_task_manager_instance()
+            .Get_current_task_identifier()
+            .map_err(|_| Error_type::Failed_to_get_task_informations)?;
 
-    Is_a_terminal.write(false);
+        if Is_a_terminal.is_null() {
+            return Err(Error_type::Invalid_input);
+        }
 
-    0
+        *Is_a_terminal = Get_file_system_instance().Is_a_terminal(File, Task_identifier)?;
+
+        Ok(())
+    })
 }
 
 #[no_mangle]
@@ -251,10 +273,10 @@ pub extern "C" fn Xila_file_system_is_stdin(File: Unique_file_identifier_type) -
     println!(
         "Checking if file is stdin : {:?} : {:?}",
         File,
-        File == File_identifier_type::Stdin
+        File == File_identifier_type::Standard_in
     );
 
-    File == File_identifier_type::Stdin
+    File == File_identifier_type::Standard_in
 }
 
 #[no_mangle]
@@ -264,10 +286,10 @@ pub extern "C" fn Xila_file_system_is_stderr(File: Unique_file_identifier_type) 
     println!(
         "Checking if file is stderr : {:?} : {:?}",
         File,
-        File == File_identifier_type::Stderr
+        File == File_identifier_type::Standard_error
     );
 
-    File == File_identifier_type::Stderr
+    File == File_identifier_type::Standard_error
 }
 
 #[no_mangle]
@@ -277,10 +299,10 @@ pub extern "C" fn Xila_file_system_is_stdout(File: Unique_file_identifier_type) 
     println!(
         "Checking if file is stdout : {:?} : {:?}",
         File,
-        File == File_identifier_type::Stdout
+        File == File_identifier_type::Standard_out
     );
 
-    File == File_identifier_type::Stdout
+    File == File_identifier_type::Standard_out
 }
 
 /// This function is used to open a file.
@@ -289,8 +311,7 @@ pub extern "C" fn Xila_file_system_is_stdout(File: Unique_file_identifier_type) 
 ///
 /// This function is unsafe because it dereferences raw pointers.
 #[no_mangle]
-pub unsafe extern "C" fn Xila_file_system_open_at(
-    Current_file: Unique_file_identifier_type,
+pub unsafe extern "C" fn Xila_file_system_open(
     Path: *const i8,
     Mode: Mode_type,
     Open: Open_type,
@@ -302,11 +323,17 @@ pub unsafe extern "C" fn Xila_file_system_open_at(
             .to_str()
             .map_err(|_| Error_type::Invalid_input)?;
 
-        println!("Opening file : {:?}", Path);
-        println!("Current file : {:?}", Current_file);
-        println!("Mode : {:?}", Mode);
-        println!("Open : {:?}", Open);
-        println!("Status : {:?}", Status);
+        let Flags = Flags_type::New(Mode, Some(Open), Some(Status));
+
+        println!("Opening file : {:?} with flags : {:?}", Path, Flags);
+
+        let Task = Get_task_manager_instance()
+            .Get_current_task_identifier()
+            .map_err(|_| Error_type::Failed_to_get_task_informations)?;
+
+        *File = Get_file_system_instance()
+            .Open(&Path, Flags, Task)
+            .expect("Failed to open file");
 
         Ok(())
     })
@@ -341,39 +368,197 @@ pub unsafe extern "C" fn Xila_file_system_resolve_path(
     })
 }
 
-/// This function is used to open a file.
+/// This function is used to open a directory.
 ///
 /// # Safety
 ///
 /// This function is unsafe because it dereferences raw pointers.
-///
-/// # Errors
-///
-/// This function may return an error if the file system fails to open the file.
 #[no_mangle]
-pub unsafe extern "C" fn Xila_file_system_open(
-    Path: *const i8,
-    Mode: Mode_type,
-    Open: Open_type,
-    Status: Status_type,
-    File: *mut Unique_file_identifier_type,
+pub unsafe extern "C" fn Xila_file_system_open_directory(
+    Path: *const c_char,
+    Directory: *mut Unique_file_identifier_type,
 ) -> u32 {
     Into_u32(move || {
-        let Path = std::ffi::CStr::from_ptr(Path)
+        let Path = CStr::from_ptr(Path)
             .to_str()
             .map_err(|_| Error_type::Invalid_input)?;
 
-        let Task_identifier = Get_task_manager_instance()
+        println!("Opening directory : {:?}", Path);
+
+        let Task = Get_task_manager_instance()
             .Get_current_task_identifier()
             .map_err(|_| Error_type::Failed_to_get_task_informations)?;
 
-        *File = Get_file_system_instance()
-            .Open(
-                &Path,
-                Flags_type::New(Mode, Some(Open), Some(Status)),
-                Task_identifier,
-            )
-            .expect("Failed to open file.");
+        *Directory = Get_file_system_instance()
+            .Open_directory(&Path, Task)
+            .expect("Failed to open directory");
+
+        println!("Directory : {:?}", *Directory);
+
+        Ok(())
+    })
+}
+
+/// This function is used to read a directory.
+///
+/// # Safety
+///
+/// This function is unsafe because it dereferences raw pointers.
+#[no_mangle]
+pub unsafe extern "C" fn Xila_file_system_read_directory(
+    File: Unique_file_identifier_type,
+    Entry_name: *mut *mut i8,
+    Entry_type: *mut Type_type,
+    Entry_size: *mut Size_type,
+    Entry_inode: *mut Inode_type,
+) -> u32 {
+    println!("Reading directory : {:?}", File);
+
+    Into_u32(move || {
+        let Task = Get_task_manager_instance()
+            .Get_current_task_identifier()
+            .map_err(|_| Error_type::Failed_to_get_task_informations)?;
+
+        let Entry = Get_file_system_instance().Read_directory(File, Task)?;
+
+        if let Some(Entry) = Entry {
+            *Entry_name = CString::new(Entry.Get_name().as_str()).unwrap().into_raw();
+            *Entry_type = Entry.Get_type();
+            *Entry_size = Entry.Get_size();
+            *Entry_inode = Entry.Get_inode();
+        } else {
+            *Entry_name = null_mut();
+        }
+
+        Ok(())
+    })
+}
+
+#[no_mangle]
+pub extern "C" fn Xila_file_system_close_directory(Directory: Unique_file_identifier_type) -> u32 {
+    Into_u32(move || {
+        let Task = Get_task_manager_instance()
+            .Get_current_task_identifier()
+            .map_err(|_| Error_type::Failed_to_get_task_informations)?;
+
+        Get_file_system_instance().Close_directory(Directory, Task)?;
+
+        Ok(())
+    })
+}
+
+#[no_mangle]
+pub extern "C" fn Xila_file_system_rewind_directory(Directory: Unique_file_identifier_type) -> u32 {
+    Into_u32(move || {
+        let Task = Get_task_manager_instance()
+            .Get_current_task_identifier()
+            .map_err(|_| Error_type::Failed_to_get_task_informations)?;
+
+        Get_file_system_instance().Rewind_directory(Directory, Task)?;
+
+        Ok(())
+    })
+}
+
+#[no_mangle]
+pub extern "C" fn Xila_file_system_flush(File: Unique_file_identifier_type, _: bool) -> u32 {
+    Into_u32(move || {
+        let Task = Get_task_manager_instance()
+            .Get_current_task_identifier()
+            .map_err(|_| Error_type::Failed_to_get_task_informations)?;
+
+        Get_file_system_instance().Flush(File, Task)?;
+
+        Ok(())
+    })
+}
+
+#[no_mangle]
+pub extern "C" fn Xila_file_system_read_link_at(
+    _Directory: Unique_file_identifier_type,
+    _Path: *mut i8,
+    _Size: usize,
+    _Used: *mut usize,
+) -> u32 {
+    todo!()
+}
+
+/// This function is used to set the position in a file.
+///
+/// # Safety
+///
+/// This function is unsafe because it dereferences raw pointers.
+#[no_mangle]
+pub unsafe extern "C" fn Xila_file_system_set_position(
+    File: Unique_file_identifier_type,
+    Offset: i64,
+    Whence: Whence_type,
+    Position: *mut Size_type,
+) -> u32 {
+    Into_u32(move || {
+        let Task = Get_task_manager_instance()
+            .Get_current_task_identifier()
+            .map_err(|_| Error_type::Failed_to_get_task_informations)?;
+
+        let Current_position = Position_type::From_whence(Whence, Offset);
+
+        println!(
+            "Setting position : {:?} : {:?} : {:?}",
+            File, Current_position, Position
+        );
+
+        *Position = Get_file_system_instance().Set_position(File, &Current_position, Task)?;
+
+        Ok(())
+    })
+}
+
+/// This function is used to get the position in a file.
+///
+/// # Safety
+///
+/// This function is unsafe because it dereferences raw pointers.
+#[no_mangle]
+pub unsafe extern "C" fn Xila_file_system_create_directory(Path: *const c_char) -> u32 {
+    Into_u32(move || {
+        let Path = CStr::from_ptr(Path)
+            .to_str()
+            .map_err(|_| Error_type::Invalid_input)?;
+
+        println!("Creating directory : {:?}", Path);
+
+        let Task = Get_task_manager_instance()
+            .Get_current_task_identifier()
+            .map_err(|_| Error_type::Failed_to_get_task_informations)?;
+
+        Get_file_system_instance().Create_directory(&Path, Task)?;
+
+        Ok(())
+    })
+}
+
+/// This function is used to rename (move) a file.
+///
+/// # Safety
+///
+/// This function is unsafe because it dereferences raw pointers.
+#[no_mangle]
+pub unsafe extern "C" fn Xila_file_system_rename(
+    Old_path: *const c_char,
+    New_path: *const c_char,
+) -> u32 {
+    Into_u32(move || {
+        let Old_path = CStr::from_ptr(Old_path)
+            .to_str()
+            .map_err(|_| Error_type::Invalid_input)?;
+
+        let New_path = CStr::from_ptr(New_path)
+            .to_str()
+            .map_err(|_| Error_type::Invalid_input)?;
+
+        println!("Renaming : {:?} to : {:?}", Old_path, New_path);
+
+        Get_file_system_instance().Rename(&Old_path, &New_path)?;
 
         Ok(())
     })
