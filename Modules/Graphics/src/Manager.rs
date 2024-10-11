cdc9b0ca8fedd0b3406f30f4f4809819a766c29d
Alix ANNERAUD <alix@anneraud.fr>
2024-10-11 10:02:28 +0200

    Use device directly for screen and pointer drivers for optimization purpose

--- Modules/Graphics/src/Manager.rs
+++ Modules/Graphics/src/Manager.rs
@@ -1,10 +1,11 @@
 use std::sync::Mutex;
 use std::time::Duration;
 
+use File_system::Device_type;
+
 use super::lvgl;
 
 use super::Point_type;
-use Virtual_file_system::File_type;
 
 use crate::Display_type;
 use crate::{Error_type, Input_type, Result_type, Screen_read_data_type};
@@ -76,21 +77,21 @@ impl Manager_type {
 
     pub fn Create_display<const Buffer_size: usize>(
         &self,
-        Screen_file: File_type,
-        Pointer_file: File_type,
+        Screen_device: Device_type,
+        Pointer_device: Device_type,
         Double_buffered: bool,
     ) -> Result_type<Display_type<Buffer_size>> {
         let mut Screen_read_data = Screen_read_data_type::default();
 
-        Screen_file
+        Screen_device
             .Read(Screen_read_data.as_mut())
             .map_err(|_| Error_type::Failed_to_get_resolution)?;
 
         let Resolution: Point_type = Screen_read_data.Get_resolution();
 
-        let Display = Display_type::New(Screen_file, Resolution, Double_buffered)?;
+        let Display = Display_type::New(Screen_device, Resolution, Double_buffered)?;
 
-        let Input = Input_type::New(Pointer_file, &Display)?;
+        let Input = Input_type::New(Pointer_device, &Display)?;
 
         self.0.lock()?.0.replace(Input);
 
