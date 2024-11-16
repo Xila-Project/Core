6133a0f2de88af83ad7e8037f112d2386827d2bc
Alix ANNERAUD <alix@anneraud.fr>
2024-11-16 19:36:24 +0100

    Replace SDL2 window drivers with pixels and winit due to multi threading issues

--- Modules/Drivers/src/Native/Devices/mod.rs
+++ Modules/Drivers/src/Native/Devices/mod.rs
@@ -1,5 +1,5 @@
 mod Drive_file;
-mod SDL2;
+pub mod Window_screen;
 
 pub use Drive_file::*;
 use Graphics::Point_type;
@@ -9,20 +9,20 @@ pub use SDL2::*;
 
 pub fn Mount_devices(
     Task: Task_identifier_type,
-    Virtual_file_systems: &Virtual_file_system_type,
+    Virtual_file_system: &Virtual_file_system_type,
 ) -> Result<(), String> {
     const Resolution: Point_type = Point_type::New(800, 600);
 
-    let (Screen_device, Pointer_device) =
-        New_touchscreen(Resolution).expect("Error creating touchscreen");
+    let (Screen, Pointer) = Window_screen::New(Resolution)?;
 
-    Virtual_file_systems
-        .Mount_device(Task, "/Devices/Pointer", Pointer_device, false)
-        .expect("Error adding pointer device");
+    Virtual_file_system
+        .Mount_device(Task, "/Devices/Screen", Screen)
+        .map_err(|Error| format!("Error adding screen device: {:?}", Error))?;
 
-    Virtual_file_systems
-        .Mount_device(Task, "/Devices/Screen", Screen_device, false)
-        .expect("Error adding screen device");
+    Virtual_file_system
+        .Mount_device(Task, "/Devices/Pointer", Pointer)
+        .map_err(|Error| format!("Error adding pointer device: {:?}", Error))?;
+
 
     Ok(())
 }
