f896b591c75f3a3809baef5763ef24a0a5a73a0f
Alix ANNERAUD <alix@anneraud.fr>
2024-12-28 16:46:01 +0100

    Update WASM bindings visibility for specific target architecture

--- src/lib.rs
+++ src/lib.rs
@@ -2,5 +2,5 @@
 #![allow(non_camel_case_types)]
 #![allow(non_upper_case_globals)]
 
-#[cfg(feature = "WASM")]
-use WASM_bindings::*;
+#[cfg(all(target_arch = "wasm32", feature = "WASM"))]
+pub use WASM_bindings::*;
