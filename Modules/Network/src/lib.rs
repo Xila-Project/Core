fc51f801cc3061f018d0d180b9c49080435e58fd
Alix ANNERAUD <alix@anneraud.fr>
2025-02-14 20:44:09 +0100

    Remove unused Device module from Network

--- Modules/Network/src/lib.rs
+++ Modules/Network/src/lib.rs
@@ -2,14 +2,12 @@
 #![allow(non_camel_case_types)]
 #![allow(non_upper_case_globals)]
 
-mod Device;
 mod Error;
 mod IP;
 mod Protocol;
 mod Service;
 mod Traits;
 
-pub use Device::*;
 pub use Error::*;
 pub use Protocol::*;
 pub use Service::*;
