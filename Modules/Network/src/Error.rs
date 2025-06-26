1d1dd17b44ce5b6a9caefc4a233ff2df0fac4240
Alix ANNERAUD <alix@anneraud.fr>
2025-06-26 23:04:34 +0200

    Enhance error handling by implementing Display trait for Error_type in Network and Virtual_file_system modules

--- Modules/Network/src/Error.rs
+++ Modules/Network/src/Error.rs
@@ -1,4 +1,4 @@
-use core::num::NonZeroU8;
+use core::{fmt::Display, num::NonZeroU8};
 
 pub type Result_type<T> = Result<T, Error_type>;
 
@@ -47,5 +47,47 @@ impl Error_type {
 impl From<Error_type> for NonZeroU8 {
     fn from(Value: Error_type) -> Self {
         Value.Get_discriminant()
+    }
+}
+
+impl Display for Error_type {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        match self {
+            Error_type::Not_found => write!(f, "Not found"),
+            Error_type::Permission_denied => write!(f, "Permission denied"),
+            Error_type::Connection_refused => write!(f, "Connection refused"),
+            Error_type::Connection_reset => write!(f, "Connection reset"),
+            Error_type::Host_unreachable => write!(f, "Host unreachable"),
+            Error_type::Network_unreachable => write!(f, "Network unreachable"),
+            Error_type::Connection_aborted => write!(f, "Connection aborted"),
+            Error_type::Not_connected => write!(f, "Not connected"),
+            Error_type::Address_in_use => write!(f, "Address in use"),
+            Error_type::Address_not_available => write!(f, "Address not available"),
+            Error_type::Network_down => write!(f, "Network down"),
+            Error_type::Broken_pipe => write!(f, "Broken pipe"),
+            Error_type::Already_exists => write!(f, "Already exists"),
+            Error_type::Would_block => write!(f, "Would block"),
+            Error_type::Invalid_input => write!(f, "Invalid input"),
+            Error_type::Invalid_data => write!(f, "Invalid data"),
+            Error_type::Timed_out => write!(f, "Timed out"),
+            Error_type::Write_zero => write!(f, "Write zero"),
+            Error_type::Storage_full => write!(f, "Storage full"),
+            Error_type::Resource_busy => write!(f, "Resource busy"),
+            Error_type::Deadlock => write!(f, "Deadlock"),
+            Error_type::Interrupted => write!(f, "Interrupted"),
+            Error_type::Unsupported => write!(f, "Unsupported operation"),
+            Error_type::Unexpected_end_of_file => write!(f, "Unexpected end of file"),
+            Error_type::Out_of_memory => write!(f, "Out of memory"),
+            Error_type::In_progress => write!(f, "In progress operation not completed yet"),
+            Error_type::Poisonned_lock => write!(f, "Poisoned lock encountered an error state"),
+            Error_type::Unsupported_protocol => write!(f, "Unsupported protocol used in operation"),
+            Error_type::Invalid_identifier => {
+                write!(f, "Invalid identifier provided for operation")
+            }
+            Error_type::Duplicate_identifier => {
+                write!(f, "Duplicate identifier found in operation")
+            }
+            Error_type::Other => write!(f, "Other error occurred"),
+        }
     }
 }
