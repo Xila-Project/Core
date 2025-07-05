daa24d78c19046990b4537a8600fff38ab2ebd2f
Alix ANNERAUD <alix@anneraud.fr>
2025-07-05 12:39:32 +0200

    Refactor Cargo.toml and lib.rs: update dependencies and enhance module exports for Host feature

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,44 @@
+#![no_std]
 #![allow(non_snake_case)]
 #![allow(non_camel_case_types)]
 #![allow(non_upper_case_globals)]
 
 #[cfg(all(target_arch = "wasm32", feature = "WASM"))]
 pub use WASM_bindings::*;
+
+#[cfg(feature = "Host")]
+pub use Authentication;
+#[cfg(feature = "Host")]
+pub use Drivers;
+#[cfg(feature = "Host")]
+pub use Executable;
+#[cfg(feature = "Host")]
+pub use File_system;
+#[cfg(feature = "Host")]
+pub use Futures;
+#[cfg(feature = "Host")]
+pub use Graphics;
+#[cfg(feature = "Host")]
+pub use Host_bindings;
+#[cfg(feature = "Host")]
+pub use LittleFS;
+#[cfg(feature = "Host")]
+pub use Log;
+#[cfg(feature = "Host")]
+pub use Memory;
+#[cfg(feature = "Host")]
+pub use Shared;
+#[cfg(feature = "Host")]
+pub use Synchronization;
+#[cfg(feature = "Host")]
+pub use Task;
+#[cfg(feature = "Host")]
+pub use Time;
+#[cfg(feature = "Host")]
+pub use Users;
+#[cfg(feature = "Host")]
+pub use Virtual_file_system;
+#[cfg(feature = "Host")]
+pub use Virtual_machine;
+#[cfg(feature = "Host")]
+pub use ABI;
