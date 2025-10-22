9cd7ab7d1f46010593e457b52a63ea62fbd0495f
Alix ANNERAUD <alix@anneraud.fr>
2025-10-23 00:35:32 +0200

    refactor: remove unused format_identifier function and associated constants

--- modules/bindings/utilities/src/format.rs
+++ modules/bindings/utilities/src/format.rs
@@ -29,34 +29,6 @@ pub fn format_c(file_path: &Path) -> Result<(), String> {
     Ok(())
 }
 
-const NAMES: [(&str, &str); 1] = [("obj", "object")];
-
-pub fn format_identifier(prefix: &str, function_name: &str) -> String {
-    // - Remove prefix
-    let function_name = if function_name.starts_with("lv_") {
-        function_name.replacen("lv_", "", 1)
-    } else {
-        function_name.to_string()
-    };
-
-    let function_name = if !function_name.starts_with(prefix) {
-        format!("{prefix}{function_name}")
-    } else {
-        function_name
-    };
-
-    // - Replace names
-
-    function_name
-        .split("_")
-        .map(|part| match NAMES.iter().find(|(old, _)| *old == part) {
-            Some((_, new)) => new.to_string(),
-            None => part.to_string(),
-        })
-        .collect::<Vec<String>>()
-        .join("_")
-}
-
 pub fn snake_to_upper_camel_case(input: &str) -> String {
     input
         .split('_')
