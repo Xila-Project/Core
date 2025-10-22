724f20cb6f0a5e91f8b2103390c8c0884c760d25
Alix ANNERAUD <alix@anneraud.fr>
2025-10-23 00:49:24 +0200

    refactor: standardize feature names to lowercase in Cargo.toml and related files

--- src/lib.rs
+++ src/lib.rs
@@ -1,45 +1,45 @@
 #![no_std]
 
-#[cfg(all(target_arch = "wasm32", feature = "WASM"))]
+#[cfg(all(target_arch = "wasm32", feature = "wasm"))]
 pub use wasm_bindings as bindings;
 
-#[cfg(feature = "Host")]
+#[cfg(feature = "host")]
 pub use abi_context;
-#[cfg(feature = "Host")]
+#[cfg(feature = "host")]
 pub use abi_declarations;
 #[cfg(feature = "abi_definitions")]
 pub use abi_definitions;
-#[cfg(feature = "Host")]
+#[cfg(feature = "host")]
 pub use authentication;
-#[cfg(feature = "Host")]
+#[cfg(feature = "host")]
 pub use drivers;
-#[cfg(feature = "Host")]
+#[cfg(feature = "host")]
 pub use executable;
-#[cfg(feature = "Host")]
+#[cfg(feature = "host")]
 pub use file_system;
-#[cfg(feature = "Host")]
+#[cfg(feature = "host")]
 pub use futures;
-#[cfg(feature = "Host")]
+#[cfg(feature = "host")]
 pub use graphics;
 #[cfg(feature = "virtual_machine")]
 pub use host_bindings;
-#[cfg(feature = "Host")]
+#[cfg(feature = "host")]
 pub use little_fs;
-#[cfg(feature = "Host")]
+#[cfg(feature = "host")]
 pub use log;
-#[cfg(feature = "Host")]
+#[cfg(feature = "host")]
 pub use memory;
-#[cfg(feature = "Host")]
+#[cfg(feature = "host")]
 pub use shared;
-#[cfg(feature = "Host")]
+#[cfg(feature = "host")]
 pub use synchronization;
-#[cfg(feature = "Host")]
+#[cfg(feature = "host")]
 pub use task;
-#[cfg(feature = "Host")]
+#[cfg(feature = "host")]
 pub use time;
-#[cfg(feature = "Host")]
+#[cfg(feature = "host")]
 pub use users;
-#[cfg(feature = "Host")]
+#[cfg(feature = "host")]
 pub use virtual_file_system;
 #[cfg(feature = "virtual_machine")]
 pub use virtual_machine;
