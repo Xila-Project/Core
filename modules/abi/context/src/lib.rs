8baee91db9b847784c84cb033a507179fb7854f6
Alix ANNERAUD <alix@anneraud.fr>
2025-10-27 16:18:26 +0100

    Implement opened file identifier path management in Context struct

--- modules/abi/context/src/lib.rs
+++ modules/abi/context/src/lib.rs
@@ -1,4 +1,11 @@
+#![no_std]
+
+extern crate alloc;
+
+use alloc::{collections::btree_map::BTreeMap, vec, vec::Vec};
+use file_system::{Path, PathOwned, UniqueFileIdentifier};
 use futures::block_on;
+use smol_str::SmolStr;
 use synchronization::{blocking_mutex::raw::CriticalSectionRawMutex, rwlock::RwLock};
 use task::TaskIdentifier;
 
@@ -10,6 +17,8 @@ pub fn get_instance() -> &'static Context {
 
 struct Inner {
     task: Option<TaskIdentifier>,
+    opened_file_identifiers_paths:
+        BTreeMap<(TaskIdentifier, UniqueFileIdentifier), (SmolStr, UniqueFileIdentifier)>,
 }
 
 pub struct Context(RwLock<CriticalSectionRawMutex, Inner>);
@@ -22,11 +31,82 @@ impl Default for Context {
 
 impl Context {
     pub const fn new() -> Self {
-        Self(RwLock::new(Inner { task: None }))
+        Self(RwLock::new(Inner {
+            task: None,
+            opened_file_identifiers_paths: BTreeMap::new(),
+        }))
     }
 
     pub fn get_current_task_identifier(&self) -> TaskIdentifier {
         block_on(self.0.read()).task.expect("No current task set")
+    }
+
+    pub fn insert_opened_file_identifier_path(
+        &self,
+        task: TaskIdentifier,
+        file_identifier: UniqueFileIdentifier,
+        parent_file_identifier: Option<UniqueFileIdentifier>,
+        path: impl AsRef<Path>,
+    ) {
+        let mut inner = block_on(self.0.write());
+
+        let parent_file_identifier =
+            parent_file_identifier.unwrap_or(UniqueFileIdentifier::INVALID_FILE_IDENTIFIER);
+
+        inner.opened_file_identifiers_paths.insert(
+            (task, file_identifier),
+            (SmolStr::new(path.as_ref()), parent_file_identifier),
+        );
+    }
+
+    pub fn remove_opened_file_identifier_path(
+        &self,
+        task: TaskIdentifier,
+        file_identifier: UniqueFileIdentifier,
+    ) {
+        let mut inner = block_on(self.0.write());
+        inner
+            .opened_file_identifiers_paths
+            .remove(&(task, file_identifier));
+    }
+
+    pub fn get_full_path(
+        &self,
+        task: TaskIdentifier,
+        file_identifier: UniqueFileIdentifier,
+        path: impl AsRef<Path>,
+    ) -> Option<PathOwned> {
+        let inner = block_on(self.0.read());
+
+        let mut stack: Vec<&SmolStr> = vec![];
+
+        let mut new_size = path.as_ref().get_length();
+
+        let mut current_file_identifier = file_identifier;
+
+        while let Some((path, parent_file_identifier)) = inner
+            .opened_file_identifiers_paths
+            .get(&(task, current_file_identifier))
+        {
+            new_size += path.len() + 1; // +1 for the separator
+
+            if *parent_file_identifier == UniqueFileIdentifier::INVALID_FILE_IDENTIFIER {
+                break;
+            }
+
+            stack.push(path);
+            current_file_identifier = *parent_file_identifier;
+        }
+
+        let mut new_path = PathOwned::new_with_capacity(new_size);
+
+        while let Some(path) = stack.pop() {
+            new_path = new_path.join(Path::from_str(path))?;
+        }
+
+        let new_path = new_path.join(path)?;
+
+        Some(new_path)
     }
 
     pub async fn set_task(&self, task: TaskIdentifier) {
