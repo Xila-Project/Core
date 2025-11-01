78b348fd51f63f85806d00f903d62b1e52912f4a
Alix ANNERAUD <alix@anneraud.fr>
2025-11-01 20:00:02 +0100

    Refactor wasm_test to improve code clarity and update bindings usage

--- modules/bindings/host/tests/wasm_test/src/main.rs
+++ modules/bindings/host/tests/wasm_test/src/main.rs
@@ -1,64 +1,48 @@
-
-
-
-
-use wasm_bindings::*;
-
-use std::thread::sleep;
-
+#[cfg(target_arch = "wasm32")]
 fn main() {
+    use std::thread::sleep;
+
     println!("Hello, world!");
 
-    let mut window = xila_graphics_object_t::MAX;
-    let mut button = xila_graphics_object_t::MAX;
-    let mut label = xila_graphics_object_t::MAX;
-
     unsafe {
-        println!("Window : {:x}", window);
+        let window = wasm_bindings::window_create().unwrap();
 
-        xila_graphics_window_create(&mut window as *mut _);
+        let button = wasm_bindings::button_create(window).unwrap();
 
-        println!("Window : {:x}", window);
-
-        xila_graphics_button_create(window, &mut button as *mut _);
-
-        println!("Button : {:x}", button);
-
-        xila_graphics_label_create(button, &mut label as *mut _);
-        xila_graphics_label_set_text(label, c"Hello, world!".as_ptr() as *mut _);
-
-        println!("Label : {:x}", label);
+        let label = wasm_bindings::label_create(button).unwrap();
+        wasm_bindings::label_set_text(label, c"Hello, world!".as_ptr() as *mut _).unwrap();
 
         loop {
-            let mut Code = xila_graphics_event_code_t_LV_EVENT_ALL;
+            let mut code = wasm_bindings::EventCode::All;
+            let mut target: *mut wasm_bindings::Object = core::ptr::null_mut();
 
-            xila_graphics_window_get_event_code(window, &mut Code as *mut _);
+            let _ = wasm_bindings::window_pop_event(
+                window,
+                &mut code as *mut _ as *mut _,
+                &mut target as *mut _ as *mut _,
+            );
 
-            if Code != xila_graphics_event_code_t_LV_EVENT_ALL {
-                match Code {
-                    xila_graphics_event_code_t_LV_EVENT_CLICKED => {
-                        println!("Button pressed!");
+            match code {
+                wasm_bindings::EventCode::Clicked => {
+                    println!("Button pressed!");
 
-                        let mut Target = xila_graphics_object_t::MAX;
-
-                        xila_graphics_window_get_event_target(window, &mut Target as *mut _);
-
-                        if Target == button {
-                            xila_graphics_label_set_text(
-                                label,
-                                c"Button pressed!".as_ptr() as *mut _,
-                            );
-                        }
-                    }
-                    Event => {
-                        println!("Event : {}", Event);
+                    if target == button {
+                        wasm_bindings::label_set_text(label, c"Button pressed!".as_ptr() as *mut _)
+                            .unwrap();
                     }
                 }
-
-                xila_graphics_window_next_event(window);
+                wasm_bindings::EventCode::All => {}
+                event => {
+                    println!("Event : {:?}", event);
+                }
             }
 
             sleep(std::time::Duration::from_millis(10));
         }
     }
+}
+
+#[cfg(not(target_arch = "wasm32"))]
+fn main() {
+    println!("This test is only for the wasm32 target.");
 }
