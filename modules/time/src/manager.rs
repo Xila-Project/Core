a4d78ae7cc6067f00d204cc2555e8e4321dbe05c
Alix ANNERAUD <alix@anneraud.fr>
2025-11-23 19:16:59 +0100

    refactor: update Manager to use DirectCharacterDevice for improved type safety and clarity

--- modules/time/src/manager.rs
+++ modules/time/src/manager.rs
@@ -1,30 +1,30 @@
-use std::sync::OnceLock;
-
-use core::time::Duration;
-use file_system::Device;
-
 use crate::{Error, Result};
+use core::time::Duration;
+use file_system::DirectCharacterDevice;
+use synchronization::once_lock::OnceLock;
 
 pub static MANAGER: OnceLock<Manager> = OnceLock::new();
 
-pub fn get_instance() -> &'static Manager {
-    MANAGER.get().expect("Time manager is not initialized")
+pub fn get_instance() -> &'static Manager<'static> {
+    MANAGER.try_get().expect("Time manager is not initialized")
 }
 
-pub fn initialize(driver: Device) -> Result<&'static Manager> {
+pub fn initialize(
+    driver: &'static (dyn DirectCharacterDevice + Send + Sync),
+) -> Result<&'static Manager<'static>> {
     MANAGER.get_or_init(|| Manager::new(driver).expect("Failed to initialize time manager"));
 
     Ok(get_instance())
 }
 
-pub struct Manager {
-    device: Device,
+pub struct Manager<'a> {
+    device: &'a (dyn DirectCharacterDevice + Send + Sync),
     start_time: Duration,
 }
 
-impl Manager {
-    pub fn new(device: Device) -> Result<Self> {
-        let start_time = Self::get_current_time_from_device(&device)?;
+impl<'a> Manager<'a> {
+    pub fn new(device: &'a (dyn DirectCharacterDevice + Send + Sync)) -> Result<Self> {
+        let start_time = Self::get_current_time_from_device(device)?;
 
         Ok(Self { device, start_time })
     }
@@ -36,10 +36,10 @@ impl Manager {
     }
 
     pub fn get_current_time(&self) -> Result<Duration> {
-        Self::get_current_time_from_device(&self.device)
+        Self::get_current_time_from_device(self.device)
     }
 
-    fn get_current_time_from_device(device: &Device) -> Result<Duration> {
+    fn get_current_time_from_device(device: &dyn DirectCharacterDevice) -> Result<Duration> {
         let mut current_time = Duration::default();
 
         let current_time_raw = unsafe {
@@ -49,7 +49,9 @@ impl Manager {
             )
         };
 
-        device.read(current_time_raw).map_err(Error::DeviceError)?;
+        device
+            .read(current_time_raw, 0)
+            .map_err(Error::DeviceError)?;
 
         Ok(current_time)
     }
