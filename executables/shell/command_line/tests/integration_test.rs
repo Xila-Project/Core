cd08cd9462a3b5517c5cbb2b7f94b12da8396216
Alix ANNERAUD <alix@anneraud.fr>
2025-11-23 19:34:32 +0100

    refactor: simplify integration test by removing unused code and streamlining setup

--- executables/shell/command_line/tests/integration_test.rs
+++ executables/shell/command_line/tests/integration_test.rs
@@ -6,116 +6,28 @@ async fn main() {
 
     extern crate alloc;
 
-    use command_line_shell::ShellExecutable;
-    use xila::executable::{Standard, mount_static_executables};
-    use xila::file_system::{MemoryDevice, create_device, create_file_system};
-    use xila::users::GroupIdentifier;
-    use xila::virtual_file_system::{create_default_hierarchy, mount_static_devices};
-    use xila::{authentication, file_system::Mode, task, time, users, virtual_file_system};
+    use xila::{
+        executable::{self, mount_executables},
+        task, virtual_file_system,
+    };
 
-    let task_instance = task::initialize();
+    let standard = testing::initialize(false).await;
 
-    let _ = users::initialize();
+    let virtual_file_system = virtual_file_system::get_instance();
+    let task = task::get_instance().get_current_task_identifier().await;
 
-    let _ = time::initialize(create_device!(drivers_native::TimeDriver::new()));
-
-    let memory_device = create_device!(MemoryDevice::<512>::new(1024 * 512));
-
-    little_fs::FileSystem::format(memory_device.clone(), 256).unwrap();
-
-    let file_system = little_fs::FileSystem::new(memory_device, 256).unwrap();
-
-    let virtual_file_system =
-        virtual_file_system::initialize(create_file_system!(file_system), None).unwrap();
-
-    let task = task_instance.get_current_task_identifier().await;
-
-    create_default_hierarchy(virtual_file_system, task)
-        .await
-        .unwrap();
-
-    mount_static_devices!(
+    mount_executables!(
         virtual_file_system,
         task,
-        &[
-            (
-                &"/devices/standard_in",
-                drivers_std::console::StandardInDevice
-            ),
-            (
-                &"/devices/standard_out",
-                drivers_std::console::StandardOutDevice
-            ),
-            (
-                &"/devices/standard_error",
-                drivers_std::console::StandardErrorDevice
-            ),
-            (&"/devices/time", drivers_native::TimeDriver),
-            (&"/devices/random", drivers_shared::devices::RandomDevice),
-            (&"/devices/null", drivers_core::NullDevice)
-        ]
+        &[(
+            &"/binaries/command_line_shell",
+            command_line_shell::ShellExecutable
+        ),]
     )
     .await
     .unwrap();
 
-    mount_static_executables!(
-        virtual_file_system,
-        task,
-        &[(&"/binaries/command_line_shell", ShellExecutable)]
-    )
-    .await
-    .unwrap();
-
-    let group_identifier = GroupIdentifier::new(1000);
-
-    authentication::create_group(virtual_file_system, "alix_anneraud", Some(group_identifier))
-        .await
-        .unwrap();
-
-    authentication::create_user(
-        virtual_file_system,
-        "alix_anneraud",
-        "password",
-        group_identifier,
-        None,
-    )
-    .await
-    .unwrap();
-
-    let standard_in = virtual_file_system
-        .open(&"/devices/standard_in", Mode::READ_ONLY.into(), task)
-        .await
-        .unwrap();
-
-    let standard_out = virtual_file_system
-        .open(&"/devices/standard_out", Mode::WRITE_ONLY.into(), task)
-        .await
-        .unwrap();
-
-    let standard_error = virtual_file_system
-        .open(&"/devices/standard_error", Mode::WRITE_ONLY.into(), task)
-        .await
-        .unwrap();
-
-    let standard = Standard::new(
-        standard_in,
-        standard_out,
-        standard_error,
-        task,
-        virtual_file_system,
-    );
-
-    task_instance
-        .set_environment_variable(task, "Paths", "/")
-        .await
-        .unwrap();
-
-    task_instance
-        .set_environment_variable(task, "Host", "xila")
-        .await
-        .unwrap();
-
-    let result = xila::executable::execute("/binaries/command_line_shell", vec![], standard, None)
+    let result = executable::execute("/binaries/command_line_shell", vec![], standard, None)
         .await
         .unwrap()
         .join()
