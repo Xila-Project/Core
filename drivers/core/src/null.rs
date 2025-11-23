2bfa5b9cbc68e1873a52e3107ca78324c725041e
Alix ANNERAUD <alix@anneraud.fr>
2025-11-23 19:36:15 +0100

    refactor: update NullDevice implementation to use DirectBaseOperations and simplify methods

--- drivers/core/src/null.rs
+++ drivers/core/src/null.rs
@@ -1,25 +1,17 @@
-use file_system::{DeviceTrait, Size};
+use file_system::{CharacterDevice, DirectBaseOperations, MountOperations, Size};
 
 pub struct NullDevice;
 
-impl DeviceTrait for NullDevice {
-    fn read(&self, buffer: &mut [u8]) -> file_system::Result<file_system::Size> {
-        Ok(Size::new(buffer.len() as u64))
+impl DirectBaseOperations for NullDevice {
+    fn read(&self, buffer: &mut [u8], _: Size) -> file_system::Result<usize> {
+        Ok(buffer.len() as _)
     }
 
-    fn write(&self, buffer: &[u8]) -> file_system::Result<file_system::Size> {
-        Ok(Size::new(buffer.len() as u64))
-    }
-
-    fn get_size(&self) -> file_system::Result<file_system::Size> {
-        Ok(Size::new(0))
-    }
-
-    fn set_position(&self, _: &file_system::Position) -> file_system::Result<file_system::Size> {
-        Ok(Size::new(0))
-    }
-
-    fn flush(&self) -> file_system::Result<()> {
-        Ok(())
+    fn write(&self, buffer: &[u8], _: Size) -> file_system::Result<usize> {
+        Ok(buffer.len() as _)
     }
 }
+
+impl MountOperations for NullDevice {}
+
+impl CharacterDevice for NullDevice {}
