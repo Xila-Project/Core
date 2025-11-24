0adb943eee7478968da3a9da09dc30ce8ac2fbf9
Alix ANNERAUD <alix@anneraud.fr>
2025-11-24 12:22:22 +0100

    refactor: improve FileIdentifier handling and simplify UniqueFileIdentifier implementation

--- modules/abi/context/src/file.rs
+++ modules/abi/context/src/file.rs
@@ -4,9 +4,9 @@
 //! within a task's file descriptor table, similar to file descriptors in
 //! Unix-like systems.
 
-use task::TaskIdentifier;
-
 use crate::UniqueFileIdentifier;
+use core::num::NonZero;
+use task::TaskIdentifier;
 
 pub type FileIdentifierInner = u16;
 
@@ -30,64 +30,60 @@ pub type FileIdentifierInner = u16;
 /// use abi_context::FileIdentifier;
 ///
 /// // Standard file identifiers
-/// assert_eq!(FileIdentifier::STANDARD_IN.into_inner(), 0);
-/// assert_eq!(FileIdentifier::STANDARD_OUT.into_inner(), 1);
-/// assert_eq!(FileIdentifier::STANDARD_ERROR.into_inner(), 2);
+/// assert_eq!(1u16, FileIdentifier::STANDARD_IN.into());
+/// assert_eq!(2u16, FileIdentifier::STANDARD_OUT.into());
+/// assert_eq!(3u16, FileIdentifier::STANDARD_ERROR.into());
 ///
 /// // Create a custom file identifier
-/// let file_id = FileIdentifier::new(42);
-/// assert_eq!(file_id.into_inner(), 42);
+/// let file_id = FileIdentifier::new(42).unwrap();
+/// assert_eq!(42u16, file_id.into());
 /// ```
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, Ord, PartialOrd)]
 #[repr(transparent)]
-pub struct FileIdentifier(FileIdentifierInner);
+pub struct FileIdentifier(NonZero<u16>);
 
 impl FileIdentifier {
     /// Size in bits of the underlying identifier type.
     pub const SIZE_BITS: u8 = core::mem::size_of::<FileIdentifierInner>() as u8 * 8;
 
-    pub const DIRECTORY_FLAG: FileIdentifierInner = 1 << (Self::SIZE_BITS - 1);
+    const DIRECTORY_FLAG: FileIdentifierInner = 1 << (Self::SIZE_BITS - 1);
 
     /// Standard input file identifier (traditionally 0).
-    pub const STANDARD_IN: Self = Self::new(0);
+    pub const STANDARD_IN: Self = Self::new_panic(1);
 
     /// Standard output file identifier (traditionally 1).
-    pub const STANDARD_OUT: Self = Self::new(1);
+    pub const STANDARD_OUT: Self = Self::new_panic(2);
 
     /// Standard error file identifier (traditionally 2).
-    pub const STANDARD_ERROR: Self = Self::new(2);
+    pub const STANDARD_ERROR: Self = Self::new_panic(3);
 
     /// Minimum file identifier available for regular files.
     ///
     /// Regular files should use identifiers starting from this value to avoid
     /// conflicts with internal or reserved identifiers.
-    pub const MINIMUM_FILE: Self = Self::new(3);
+    pub const MINIMUM_FILE: Self = Self::new_panic(4);
 
     /// Maximum possible file identifier value.
-    pub const MAXIMUM_FILE: Self = Self::new(Self::DIRECTORY_FLAG - 1);
+    pub const MAXIMUM_FILE: Self = Self::new_panic(Self::DIRECTORY_FLAG - 1);
 
-    pub const MINIMUM_DIRECTORY: Self = Self::new(Self::DIRECTORY_FLAG);
+    pub const MINIMUM_DIRECTORY: Self = Self::new_panic(Self::DIRECTORY_FLAG);
 
-    pub const MAXIMUM_DIRECTORY: Self = Self::new(FileIdentifierInner::MAX - 1);
-
-    pub const INVALID: Self = Self::new(FileIdentifierInner::MAX);
-
+    pub const MAXIMUM_DIRECTORY: Self = Self::new_panic(FileIdentifierInner::MAX);
     /// Create a new file identifier from a raw value.
     ///
     /// # Arguments
     ///
-    /// * `Identifier` - The raw identifier value
-    ///
-    /// # Examples
-    ///
-    /// ```rust
-    /// use abi_context::FileIdentifier;
-    ///
-    /// let file_id = FileIdentifier::new(5);
-    /// assert_eq!(file_id.into_inner(), 5);
-    /// ```
-    pub const fn new(identifier: FileIdentifierInner) -> Self {
-        Self(identifier)
+    /// * `Identifier` - The raw identifier value.
+    pub const fn new(identifier: u16) -> Option<Self> {
+        if let Some(non_zero) = NonZero::new(identifier) {
+            Some(Self(non_zero))
+        } else {
+            None
+        }
+    }
+
+    pub const fn new_panic(identifier: u16) -> Self {
+        Self::new(identifier).expect("FileIdentifier cannot be zero")
     }
 
     /// Get the raw identifier value.
@@ -95,17 +91,8 @@ impl FileIdentifier {
     /// # Returns
     ///
     /// The underlying raw identifier value.
-    ///
-    /// # Examples
-    ///
-    /// ```rust
-    /// use abi_context::FileIdentifier;
-    ///
-    /// let file_id = FileIdentifier::new(42);
-    /// assert_eq!(file_id.into_inner(), 42);
-    /// ```
     pub const fn into_inner(self) -> FileIdentifierInner {
-        self.0
+        self.0.get()
     }
 
     pub const fn into_unique(self, task: TaskIdentifier) -> UniqueFileIdentifier {
@@ -113,30 +100,22 @@ impl FileIdentifier {
     }
 
     pub const fn is_directory(self) -> bool {
-        (self.0 & Self::DIRECTORY_FLAG) != 0
+        (self.0.get() & Self::DIRECTORY_FLAG) != 0
     }
 }
 
-impl From<FileIdentifierInner> for FileIdentifier {
-    fn from(internal_file_identifier: FileIdentifierInner) -> Self {
-        FileIdentifier(internal_file_identifier)
+impl TryFrom<FileIdentifierInner> for FileIdentifier {
+    type Error = virtual_file_system::Error;
+
+    fn try_from(value: u16) -> Result<Self, Self::Error> {
+        NonZero::new(value)
+            .map(FileIdentifier)
+            .ok_or(virtual_file_system::Error::InvalidIdentifier)
     }
 }
 
-impl From<FileIdentifier> for FileIdentifierInner {
-    fn from(internal_file_identifier: FileIdentifier) -> Self {
-        internal_file_identifier.0
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_file_identifier() {
-        let identifier = FileIdentifier::from(0x1234);
-        assert_eq!(identifier, FileIdentifier::new(0x1234));
-        assert_eq!(FileIdentifierInner::from(identifier), 0x1234);
+impl From<FileIdentifier> for u16 {
+    fn from(file_identifier: FileIdentifier) -> Self {
+        file_identifier.0.get()
     }
 }
--- modules/abi/context/src/unique_file.rs
+++ modules/abi/context/src/unique_file.rs
@@ -1,11 +1,6 @@
-use core::{
-    fmt::Debug,
-    ops::{Add, AddAssign},
-};
-
+use crate::FileIdentifier;
+use core::fmt::Debug;
 use task::{TaskIdentifier, TaskIdentifierInner};
-
-use crate::{FileIdentifier, FileIdentifierInner};
 
 #[derive(Clone, Copy, PartialEq, Eq, Hash, Ord, PartialOrd)]
 #[repr(transparent)]
@@ -38,8 +33,7 @@ impl UniqueFileIdentifier {
         let task = self.0 >> FileIdentifier::SIZE_BITS;
         let task = TaskIdentifier::new(task as TaskIdentifierInner);
 
-        let file = self.0 as FileIdentifierInner;
-        let file = FileIdentifier::new(file);
+        let file = FileIdentifier::new_panic(self.0 as _);
 
         (task, file)
     }
@@ -48,66 +42,11 @@ impl UniqueFileIdentifier {
         self.split().1
     }
 
-    pub const fn get_minimum(task: TaskIdentifier) -> Self {
-        Self::new(task, FileIdentifier::MINIMUM_FILE)
-    }
-
-    pub const fn get_maximum(task: TaskIdentifier) -> Self {
-        Self::new(task, FileIdentifier::MAXIMUM_FILE)
-    }
-
     pub const fn into_inner(self) -> usize {
         self.0
     }
 
     pub const fn from_raw(value: usize) -> Self {
         Self(value)
-    }
-}
-
-impl AddAssign<usize> for UniqueFileIdentifier {
-    fn add_assign(&mut self, rhs: usize) {
-        self.0 += rhs;
-    }
-}
-
-impl Add<usize> for UniqueFileIdentifier {
-    type Output = Self;
-
-    fn add(self, rhs: usize) -> Self::Output {
-        Self(self.0 + rhs)
-    }
-}
-
-impl IntoIterator for UniqueFileIdentifier {
-    type Item = UniqueFileIdentifier;
-    type IntoIter = UniqueFileIdentifierIterator;
-
-    fn into_iter(self) -> Self::IntoIter {
-        let (task, _) = self.split();
-
-        UniqueFileIdentifierIterator {
-            current: self,
-            end: UniqueFileIdentifier::get_maximum(task),
-        }
-    }
-}
-
-pub struct UniqueFileIdentifierIterator {
-    current: UniqueFileIdentifier,
-    end: UniqueFileIdentifier,
-}
-
-impl Iterator for UniqueFileIdentifierIterator {
-    type Item = UniqueFileIdentifier;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.current < self.end {
-            let current = self.current;
-            self.current += 1;
-            Some(current)
-        } else {
-            None
-        }
     }
 }
