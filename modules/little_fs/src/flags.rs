855214d892894cb5d5b129fe93711357751a0de9
Alix ANNERAUD <alix@anneraud.fr>
2025-11-24 19:43:12 +0100

    refactor: simplify access flag handling in convert_flags function

--- modules/little_fs/src/flags.rs
+++ modules/little_fs/src/flags.rs
@@ -6,9 +6,7 @@ pub fn convert_flags(flags: Flags) -> i32 {
 
     let mut little_fs_flags = 0;
 
-    if mode.contains(AccessFlags::Read) && mode.contains(AccessFlags::Write) {
-        little_fs_flags |= littlefs::lfs_open_flags_LFS_O_RDWR;
-    } else if mode.contains(AccessFlags::Write) {
+    if mode.contains(AccessFlags::Write) {
         little_fs_flags |= littlefs::lfs_open_flags_LFS_O_RDWR;
     } else {
         little_fs_flags |= littlefs::lfs_open_flags_LFS_O_RDONLY;
