1a8770b9f3ca5e41aeabfaa548434de98f6e6dd3
Alix ANNERAUD <alix@anneraud.fr>
2026-01-25 12:30:12 +0100

    feat: enhance error handling with additional error variants and conversions for DNS, ICMP, and UDP

--- modules/network/src/error.rs
+++ modules/network/src/error.rs
@@ -1,4 +1,5 @@
 use core::{fmt::Display, num::NonZeroU8};
+use smoltcp::socket::{dns, icmp, udp};
 
 pub type Result<T> = core::result::Result<T, Error>;
 
@@ -30,17 +31,38 @@ pub enum Error {
     Unsupported,
     UnexpectedEndOfFile,
     OutOfMemory,
-    InProgress,
-    PoisonnedLock,
+    Pending,
     UnsupportedProtocol,
     InvalidIdentifier,
     DuplicateIdentifier,
+    FailedToGenerateSeed(file_system::Error),
+    FailedToSpawnNetworkTask(task::Error),
+    // - DNS
+    InvalidName,
+    NameTooLong,
+    Failed,
+    // - Accept / Connect
+    InvalidState,
+    InvalidPort,
+    NoRoute,
+    // Udp
+    Truncated,
+    SocketNotBound,
+    PacketTooLarge,
+    InvalidEndpoint,
+
+    FailedToMountDevice(virtual_file_system::Error),
+
+    NoFreeSlot,
+
     Other,
 }
 
+impl core::error::Error for Error {}
+
 impl Error {
     pub const fn get_discriminant(&self) -> NonZeroU8 {
-        unsafe { NonZeroU8::new_unchecked(*self as u8) }
+        unsafe { *(self as *const Self as *const NonZeroU8) }
     }
 }
 
@@ -78,8 +100,7 @@ impl Display for Error {
             Error::Unsupported => write!(f, "Unsupported operation"),
             Error::UnexpectedEndOfFile => write!(f, "Unexpected end of file"),
             Error::OutOfMemory => write!(f, "Out of memory"),
-            Error::InProgress => write!(f, "In progress operation not completed yet"),
-            Error::PoisonnedLock => write!(f, "Poisoned lock encountered an error state"),
+            Error::Pending => write!(f, "In progress operation not completed yet"),
             Error::UnsupportedProtocol => write!(f, "Unsupported protocol used in operation"),
             Error::InvalidIdentifier => {
                 write!(f, "Invalid identifier provided for operation")
@@ -87,7 +108,73 @@ impl Display for Error {
             Error::DuplicateIdentifier => {
                 write!(f, "Duplicate identifier found in operation")
             }
+            Error::FailedToGenerateSeed(e) => {
+                write!(f, "Failed to generate seed: {}", e)
+            }
+            Error::FailedToSpawnNetworkTask(e) => {
+                write!(f, "Failed to spawn network task: {}", e)
+            }
+            Error::InvalidName => write!(f, "Invalid name"),
+            Error::NameTooLong => write!(f, "Name too long"),
+            Error::Failed => write!(f, "Failed"),
+            Error::InvalidState => write!(f, "Invalid state for operation"),
+            Error::InvalidPort => write!(f, "Invalid port specified"),
+            Error::NoRoute => write!(f, "No route to host"),
+            Error::Truncated => write!(f, "Truncated packet received"),
+            Error::SocketNotBound => write!(f, "Socket not bound"),
+            Error::PacketTooLarge => write!(f, "Packet too large to send"),
+            Error::InvalidEndpoint => write!(f, "Invalid endpoint specified"),
+            Error::FailedToMountDevice(e) => {
+                write!(f, "Failed to mount device: {}", e)
+            }
+            Error::NoFreeSlot => write!(f, "No free slot available"),
             Error::Other => write!(f, "Other error occurred"),
+        }
+    }
+}
+
+impl From<dns::StartQueryError> for Error {
+    fn from(e: dns::StartQueryError) -> Self {
+        match e {
+            dns::StartQueryError::InvalidName => Error::InvalidName,
+            dns::StartQueryError::NameTooLong => Error::NameTooLong,
+            dns::StartQueryError::NoFreeSlot => Error::NoFreeSlot,
+        }
+    }
+}
+
+impl From<dns::GetQueryResultError> for Error {
+    fn from(e: dns::GetQueryResultError) -> Self {
+        match e {
+            dns::GetQueryResultError::Pending => Error::Pending,
+            dns::GetQueryResultError::Failed => Error::Failed,
+        }
+    }
+}
+
+impl From<icmp::SendError> for Error {
+    fn from(e: icmp::SendError) -> Self {
+        match e {
+            icmp::SendError::Unaddressable => Error::InvalidEndpoint,
+            icmp::SendError::BufferFull => Error::ResourceBusy,
+        }
+    }
+}
+
+impl From<icmp::BindError> for Error {
+    fn from(e: icmp::BindError) -> Self {
+        match e {
+            icmp::BindError::InvalidState => Error::InvalidState,
+            icmp::BindError::Unaddressable => Error::NoRoute,
+        }
+    }
+}
+
+impl From<udp::BindError> for Error {
+    fn from(e: udp::BindError) -> Self {
+        match e {
+            udp::BindError::InvalidState => Error::InvalidState,
+            udp::BindError::Unaddressable => Error::NoRoute,
         }
     }
 }
