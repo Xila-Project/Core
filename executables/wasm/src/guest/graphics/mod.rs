ebacabd5ef6245df69fb980e42c4f9d9acf99b74
Alix ANNERAUD <alix@anneraud.fr>
2026-02-11 13:54:36 +0100

    Add c_bindings feature to WASM configuration and update graphics module

--- executables/wasm/src/guest/graphics/mod.rs
+++ executables/wasm/src/guest/graphics/mod.rs
@@ -1,5 +1,10 @@
+#[cfg(feature = "c_bindings")]
+mod c_functions;
 mod enumeration;
 mod functions;
+
+#[cfg(feature = "c_bindings")]
+pub use c_functions::*;
 
 pub use enumeration::*;
 pub use functions::*;
