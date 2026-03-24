c4081e18868ab5f4612c8d06da153bdcd93b49d9
Alix ANNERAUD <alix@anneraud.fr>
2026-03-24 12:45:40 +0100

    add derive proc macro for argument parsing with enhanced field configuration

--- modules/executable/macros/src/lib.rs
+++ modules/executable/macros/src/lib.rs
@@ -1,91 +1,327 @@
 use proc_macro::TokenStream;
-use quote::quote;
-use syn::{Data, DeriveInput, Fields, Meta, parse_macro_input};
+use quote::{format_ident, quote};
+use syn::{
+    Data, DeriveInput, Expr, Field, Fields, GenericParam, LitChar, LitStr, Type, parse_macro_input,
+    spanned::Spanned,
+};
 
-#[proc_macro_derive(GetArgs, attributes(default))]
+fn is_bool_type(ty: &Type) -> bool {
+    match ty {
+        Type::Path(path) => path
+            .path
+            .segments
+            .last()
+            .map(|segment| segment.ident == "bool")
+            .unwrap_or(false),
+        _ => false,
+    }
+}
+
+fn is_str_reference_type(ty: &Type) -> bool {
+    match ty {
+        Type::Reference(reference) => match reference.elem.as_ref() {
+            Type::Path(path) => path
+                .path
+                .segments
+                .last()
+                .map(|segment| segment.ident == "str")
+                .unwrap_or(false),
+            _ => false,
+        },
+        _ => false,
+    }
+}
+
+fn to_kebab_case(name: &str) -> String {
+    name.replace('_', "-")
+}
+
+#[derive(Default)]
+struct FieldConfig {
+    positional: bool,
+    flag: bool,
+    short: Option<char>,
+    long: Option<String>,
+    default: Option<Expr>,
+    legacy_default_used: bool,
+}
+
+fn parse_field_config(field: &Field) -> syn::Result<FieldConfig> {
+    let mut config = FieldConfig::default();
+
+    for attr in &field.attrs {
+        if attr.path().is_ident("arg") {
+            attr.parse_nested_meta(|meta| {
+                if meta.path.is_ident("positional") {
+                    config.positional = true;
+                    return Ok(());
+                }
+
+                if meta.path.is_ident("flag") {
+                    config.flag = true;
+                    return Ok(());
+                }
+
+                if meta.path.is_ident("short") {
+                    let value: LitChar = meta.value()?.parse()?;
+                    config.short = Some(value.value());
+                    return Ok(());
+                }
+
+                if meta.path.is_ident("long") {
+                    let value: LitStr = meta.value()?.parse()?;
+                    config.long = Some(value.value());
+                    return Ok(());
+                }
+
+                if meta.path.is_ident("default") {
+                    let value: Expr = meta.value()?.parse()?;
+                    config.default = Some(value);
+                    return Ok(());
+                }
+
+                Err(meta.error(
+                    "unsupported argument, expected one of: positional, flag, short, long, default",
+                ))
+            })?;
+        }
+
+        if attr.path().is_ident("default") {
+            config.legacy_default_used = true;
+
+            match &attr.meta {
+                syn::Meta::List(list) => {
+                    let value: Expr = syn::parse2(list.tokens.clone())?;
+                    config.default = Some(value);
+                }
+                syn::Meta::NameValue(named) => {
+                    config.default = Some(named.value.clone());
+                }
+                syn::Meta::Path(_) => {
+                    return Err(syn::Error::new(
+                        attr.span(),
+                        "#[default] requires a value, e.g. #[default(4)]",
+                    ));
+                }
+            }
+        }
+    }
+
+    Ok(config)
+}
+
+#[proc_macro_derive(GetArgs, attributes(arg, default))]
 pub fn derive_get_args(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
     let name = &input.ident;
-    let (impl_generics, ty_generics, _) = input.generics.split_for_impl();
+    let (impl_generics, ty_generics, where_clause) = input.generics.split_for_impl();
+    let lifetime_params: Vec<_> = input
+        .generics
+        .params
+        .iter()
+        .filter_map(|parameter| match parameter {
+            GenericParam::Lifetime(lifetime) => Some(lifetime.lifetime.clone()),
+            _ => None,
+        })
+        .collect();
 
     let fields = match &input.data {
         Data::Struct(data) => match &data.fields {
             Fields::Named(fields) => &fields.named,
-            _ => panic!("GetArgs only works on structs with named fields"),
+            _ => {
+                return syn::Error::new_spanned(
+                    &input,
+                    "GetArgs only works on structs with named fields",
+                )
+                .to_compile_error()
+                .into();
+            }
         },
-        _ => panic!("GetArgs only works on structs"),
+        _ => {
+            return syn::Error::new_spanned(&input, "GetArgs only works on structs")
+                .to_compile_error()
+                .into();
+        }
     };
 
-    // Prepare variable initializers and match arms
     let mut initializers = Vec::new();
-    let mut flag_arms = Vec::new();
-    let mut positional_field = None;
+    let mut option_match_arms = Vec::new();
+    let mut positional_match_arms = Vec::new();
+    let mut required_checks = Vec::new();
+    let mut field_builds = Vec::new();
+
+    let mut positional_index = 0usize;
 
     for field in fields {
-        let field_name = field.ident.as_ref().unwrap().clone();
+        let Some(field_name) = field.ident.as_ref() else {
+            return syn::Error::new_spanned(field, "GetArgs only works on named fields")
+                .to_compile_error()
+                .into();
+        };
+
         let field_type = &field.ty;
         let field_str = field_name.to_string();
-        let short_char = field_str.chars().next().unwrap();
+        let config = match parse_field_config(field) {
+            Ok(config) => config,
+            Err(error) => return error.to_compile_error().into(),
+        };
 
-        // Check for #[default(...)] attribute
-        let mut default_val = quote! { Default::default() };
-        let mut is_positional = true;
+        let derived_long = to_kebab_case(&field_str);
+        let derived_short = field_str
+            .chars()
+            .next()
+            .expect("field name should never be empty");
 
-        for attr in &field.attrs {
-            if attr.path().is_ident("default") {
-                is_positional = false;
-                if let Meta::List(list) = &attr.meta {
-                    let tokens = &list.tokens;
-                    default_val = quote! { #tokens };
+        let is_option = !config.positional
+            && (config.short.is_some()
+                || config.long.is_some()
+                || config.flag
+                || config.default.is_some()
+                || config.legacy_default_used);
+
+        let is_required = config.default.is_none() && !config.flag;
+
+        let default_value = config.default.unwrap_or_else(|| {
+            if config.flag {
+                syn::parse_quote!(false)
+            } else {
+                syn::parse_quote!(Default::default())
+            }
+        });
+
+        let value_ident = format_ident!("__value_{}", field_name);
+        let seen_ident = format_ident!("__seen_{}", field_name);
+
+        if is_option {
+            if config.flag && !is_bool_type(field_type) {
+                return syn::Error::new_spanned(
+                    field,
+                    "#[arg(flag)] can only be used with bool fields",
+                )
+                .to_compile_error()
+                .into();
+            }
+
+            let long_name = config.long.unwrap_or(derived_long);
+            let short_name = config.short.unwrap_or(derived_short);
+
+            let assign_value = if config.flag {
+                quote! {
+                    #value_ident = true;
                 }
+            } else if is_str_reference_type(field_type) {
+                quote! {
+                    let value = options
+                        .next_positional()
+                        .ok_or(crate::Error::MissingPositionalArgument(#long_name))?;
+                    #value_ident = value;
+                }
+            } else {
+                quote! {
+                    let value = options
+                        .next_positional()
+                        .ok_or(crate::Error::MissingPositionalArgument(#long_name))?;
+                    #value_ident = value.parse().map_err(|_| crate::Error::InvalidOption)?;
+                }
+            };
+
+            initializers.push(quote! {
+                let mut #value_ident: #field_type = #default_value;
+                let mut #seen_ident = false;
+            });
+
+            option_match_arms.push(quote! {
+                getargs::Arg::Short(#short_name) | getargs::Arg::Long(#long_name) => {
+                    #assign_value
+                    #seen_ident = true;
+                }
+            });
+
+            if is_required {
+                required_checks.push(quote! {
+                    if !#seen_ident {
+                        return Err(crate::Error::MissingPositionalArgument(#long_name));
+                    }
+                });
+            }
+        } else {
+            let current_index = positional_index;
+            positional_index += 1;
+
+            initializers.push(quote! {
+                let mut #value_ident: #field_type = #default_value;
+                let mut #seen_ident = false;
+            });
+
+            let assign_positional = if is_str_reference_type(field_type) {
+                quote! {
+                    #value_ident = value;
+                }
+            } else {
+                quote! {
+                    #value_ident = value.parse().map_err(|_| crate::Error::InvalidOption)?;
+                }
+            };
+
+            positional_match_arms.push(quote! {
+                #current_index => {
+                    #assign_positional
+                    #seen_ident = true;
+                }
+            });
+
+            if is_required {
+                required_checks.push(quote! {
+                    if !#seen_ident {
+                        return Err(crate::Error::MissingPositionalArgument(#field_str));
+                    }
+                });
             }
         }
 
-        if is_positional {
-            positional_field = Some(field_name.clone());
-            initializers.push(quote! { let mut #field_name: #field_type = ""; });
-        } else {
-            initializers.push(quote! { let mut #field_name: #field_type = #default_val; });
-            flag_arms.push(quote! {
-                getargs::Arg::Short(#short_char) | getargs::Arg::Long(#field_str) => {
-                    let value = options.next_positional()
-                        .ok_or(crate::Error::MissingPositionalArgument(#field_str))?;
-                    #field_name = value.parse().map_err(|_| crate::Error::InvalidOption)?;
-                }
-            });
-        }
+        field_builds.push(quote! { #field_name: #value_ident });
     }
 
-    let positional_logic = if let Some(p_field) = positional_field {
+    let positional_handler = if positional_match_arms.is_empty() {
         quote! {
-            getargs::Arg::Positional(p) => {
-                if !#p_field.is_empty() { return Err(crate::Error::InvalidNumberOfArguments); }
-                #p_field = p;
+            getargs::Arg::Positional(_) => {
+                return Err(crate::Error::InvalidNumberOfArguments);
             }
         }
     } else {
-        quote! { getargs::Arg::Positional(_) => return Err(crate::Error::InvalidOption), }
+        quote! {
+            getargs::Arg::Positional(value) => {
+                match __positional_index {
+                    #(#positional_match_arms)*
+                    _ => return Err(crate::Error::InvalidNumberOfArguments),
+                }
+                __positional_index += 1;
+            }
+        }
     };
 
-    let field_names = fields.iter().map(|f| &f.ident);
-
     let expanded = quote! {
-        impl #impl_generics #name #ty_generics {
-            pub fn parse<I>(options: &mut getargs::Options<&'a str, I>) -> Result<Self, crate::Error>
-            where I: Iterator<Item = &'a str>
+        impl #impl_generics #name #ty_generics #where_clause {
+            pub fn parse<'__getargs, I>(options: &mut getargs::Options<&'__getargs str, I>) -> core::result::Result<Self, crate::Error>
+            where
+                I: Iterator<Item = &'__getargs str>,
+                #('__getargs: #lifetime_params,)*
             {
                 #(#initializers)*
+                let mut __positional_index = 0_usize;
 
                 while let Some(argument) = options.next_arg().map_err(|_| crate::Error::InvalidOption)? {
                     match argument {
-                        #(#flag_arms)*
-                        #positional_logic
+                        #(#option_match_arms)*
+                        #positional_handler
                         _ => return Err(crate::Error::InvalidOption),
                     }
                 }
 
+                #(#required_checks)*
+
                 Ok(Self {
-                    #(#field_names),*
+                    #(#field_builds),*
                 })
             }
         }
